d40b6767507b91e2a6f3248e691be48ed2c3bd8d
nick <nick.walter@newnet.com>
2010-03-17 13:41:51 +0000

    initial setup of basic RPM building
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@12 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ release/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+#  Master build shell script
+
+cd `dirname $0`
+BUILDDIR=$PWD/stage
+ARTIFACTS_DIR=$PWD/ii_artifacts
+
+if [ -d $BUILDDIR ]; then
+  rm -rf $BUILDDIR
+fi
+
+
+echo "Master build populating BUILDDIR=$BUILDDIR"
+mkdir -p $BUILDDIR/interact/packages/conf
+
+# copy in the stuff.  to the place.  You know what I mean
+cp  ../gui/ii_artifacts/*.rpm $BUILDDIR/interact/packages/
+cp  ../mediator/ii_artifacts/*.rpm $BUILDDIR/interact/packages/
+cp  ../voice/ii_artifacts/*.rpm $BUILDDIR/interact/packages/
+
+cp  ../gui/ii_artifacts/*.uia $BUILDDIR/interact/packages/conf/
+cp  ../mediator/ii_artifacts/*.uia $BUILDDIR/interact/packages/conf/
+cp  ../voice/ii_artifacts/*.uia $BUILDDIR/interact/packages/conf/
+
+# Move iiInstall and iiLoader config files into place
+if [ -f iiInstall.cfg ]
+then
+    cp /iiInstall.cfg $BUILDDIR/interact/packages/conf/
+fi
+
+if [ -f iiLoader.cfg ]
+then
+    cp iiLoader.cfg $BUILDDIR/interact/packages/conf/
+fi
+
+if [ -z "$VERSION" ]; then
+    export VERSION="unknown"
+fi
+
+if [ -z "$RELEASE" ]; then
+    export RELEASE="`date +%Y%m%d%H%M`"
+fi
+
+
+export TOPDIR=$PWD/rpm-build
+rpmbuild -bb --buildroot $BUILDDIR listen.spec
+RPMFILE=`find $TOPDIR -name "*.rpm"`
+echo "RPMFILE=$RPMFILE"
+mv $RPMFILE $ARTIFACTS_DIR
+
+rm -rf $TOPDIR
+
+
+
