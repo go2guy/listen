f1c2b0f596eefde2e8701b690f330e4e6ff99597
rob <rob.hruska@newnet.com>
2010-03-25 14:41:30 +0000

    Added tests for Marshaller.createMarshaller().
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@79 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ controller/src/test/java/com/interact/listen/marshal/MarshallerTest.java
@@ -0,0 +1,60 @@
+package com.interact.listen.marshal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import com.interact.listen.marshal.json.JsonMarshaller;
+import com.interact.listen.marshal.xml.XmlMarshaller;
+
+import org.junit.Test;
+
+public class MarshallerTest
+{
+    @Test
+    public void test_createMarshaller_contentTypeNull_throwsMarshallerNotFoundExceptionWithMessage()
+    {
+        try
+        {
+            Marshaller.createMarshaller(null);
+            fail("Expected MarshallerNotFoundException for null content type");
+        }
+        catch(MarshallerNotFoundException e)
+        {
+            assertEquals("Marshaller not found for content type [null]", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test_createMarshaller_contentTypeStartsWithApplicationXml_returnsXmlMarshaller()
+        throws MarshallerNotFoundException
+    {
+        Marshaller marshaller = Marshaller.createMarshaller("application/xml");
+        assertTrue(marshaller instanceof XmlMarshaller);
+    }
+
+    @Test
+    public void test_createMarshaller_contentTypeStartsWithApplicationJson_returnsJsonMarshaller()
+        throws MarshallerNotFoundException
+    {
+        Marshaller marshaller = Marshaller.createMarshaller("application/xml");
+        assertTrue(marshaller instanceof JsonMarshaller);
+    }
+
+    @Test
+    public void test_createMarshaller_contentTypeUnrecognized_throwsMarshallerNotFoundExceptionWithMessage()
+        throws MarshallerNotFoundException
+    {
+        final String contentType = "turdferguson";
+
+        try
+        {
+            Marshaller.createMarshaller(contentType);
+            fail("Expected MarshallerNotFoundException for " + contentType + " content type");
+        }
+        catch(MarshallerNotFoundException e)
+        {
+            assertEquals("Marshaller not found for content type [" + contentType + "]", e.getMessage());
+        }
+    }
+}
