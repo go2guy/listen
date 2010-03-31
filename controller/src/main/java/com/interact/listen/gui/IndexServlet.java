9bc3049965acbb2fce8ad941e56dc0cfc3b3865f
rob <rob.hruska@newnet.com>
2010-03-31 20:00:43 +0000

    Reworked build.xml to build both the controller and gui rpms at the same time. Both rpms are now published to ii_artifacts. Also added a skeleton servlet and index.html to the gui webapp, and configured both the controller and gui web.xml files to be semi-correct.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@133 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ controller/src/main/java/com/interact/listen/gui/IndexServlet.java
@@ -0,0 +1,16 @@
+package com.interact.listen.webui;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    {
+        // TODO!
+    }
+}
