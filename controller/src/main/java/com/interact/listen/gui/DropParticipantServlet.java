0f2a05d21b92022ab6fa14b655cb6e56665ff983
rob <rob.hruska@newnet.com>
2010-04-05 21:23:49 +0000

    Fixed import order for DropParticipantServlet.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@179 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/src/main/java/com/interact/listen/gui/DropParticipantServlet.java
+++ controller/src/main/java/com/interact/listen/gui/DropParticipantServlet.java
@@ -6,8 +6,8 @@ import com.interact.listen.resource.Participant;
 import com.interact.listen.resource.User;
 
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
