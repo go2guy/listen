da886c3ab6b4e978570ea45e11e9387505297cf4
rob <rob.hruska@newnet.com>
2010-04-05 21:13:54 +0000

    Ability to drop participants.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@178 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ controller/src/main/java/com/interact/listen/gui/DropParticipantServlet.java
@@ -0,0 +1,68 @@
+package com.interact.listen.gui;
+
+import com.interact.listen.HibernateUtil;
+import com.interact.listen.ServletUtil;
+import com.interact.listen.resource.Participant;
+import com.interact.listen.resource.User;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class DropParticipantServlet extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    {
+        long start = System.currentTimeMillis();
+
+        User user = (User)(request.getSession().getAttribute("user"));
+        if(user == null)
+        {
+            ServletUtil.writeResponse(response, HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized", "text/plain");
+            return;
+        }
+
+        String id = request.getParameter("id");
+        if(id == null || id.trim().equals(""))
+        {
+            ServletUtil.writeResponse(response, HttpServletResponse.SC_BAD_REQUEST, "Please provide an id",
+                                      "text/plain");
+            return;
+        }
+
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.beginTransaction();
+
+        try
+        {
+            Participant participant = (Participant)session.get(Participant.class, Long.valueOf(id));
+            if(!isUserAllowedToDrop(user, participant))
+            {
+                ServletUtil.writeResponse(response, HttpServletResponse.SC_UNAUTHORIZED,
+                                          "Not allowed to drop participant", "text/plain");
+                transaction.rollback();
+                return;
+            }
+            session.delete(participant);
+            transaction.commit();
+        }
+        finally
+        {
+            System.out.println("TIMER: DropParticipantServlet.doPost() took " + (System.currentTimeMillis() - start) +
+                               "ms");
+        }
+    }
+
+    private boolean isUserAllowedToDrop(User user, Participant participant)
+    {
+        // TODO i'm not sure this is "secure" enough - however, we don't have anything directly linking "user" to
+        // "conference" right now
+        return user.getSubscriber().getNumber().equals(participant.getConference().getNumber());
+    }
+}
