fbf4eb2c5a80471a016ec26ffcea6380888088d7
rob <rob.hruska@newnet.com>
2010-04-15 20:26:18 +0000

    Added tests for GetConferenceInfoServlet.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@207 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ controller/src/test/java/com/interact/listen/gui/GetConferenceInfoServletTest.java
@@ -0,0 +1,100 @@
+package com.interact.listen.gui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.interact.listen.HibernateUtil;
+import com.interact.listen.InputStreamMockHttpServletRequest;
+import com.interact.listen.resource.Conference;
+import com.interact.listen.resource.Subscriber;
+import com.interact.listen.resource.User;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class GetConferenceInfoServletTest
+{
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    private HttpServlet servlet = new GetConferenceInfoServlet();
+
+    @Before
+    public void setUp()
+    {
+        request = new InputStreamMockHttpServletRequest();
+        response = new MockHttpServletResponse();
+    }
+
+    @Test
+    public void test_doGet_withNoSessionUser_returnsUnauthorized() throws IOException, ServletException
+    {
+        request.setMethod("GET");
+        servlet.service(request, response);
+
+        assertEquals(HttpServletResponse.SC_UNAUTHORIZED, response.getStatus());
+        assertEquals("Unauthorized", response.getContentAsString());
+    }
+
+    @Test
+    public void test_doGet_withNonexistentConference_returns500() throws IOException, ServletException
+    {
+        final Long id = System.currentTimeMillis();
+
+        Subscriber subscriber = new Subscriber();
+        subscriber.setNumber(String.valueOf(id));
+        User user = new User();
+        user.setSubscriber(subscriber);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
+
+        request.setMethod("GET");
+        servlet.service(request, response);
+
+        assertEquals(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, response.getStatus());
+        assertEquals("Conference not found", response.getContentAsString());
+    }
+
+    @Test
+    public void test_doGet_withExistingConference_returns200AndConferenceJSON() throws IOException, ServletException
+    {
+        final Long id = System.currentTimeMillis();
+
+        Subscriber subscriber = new Subscriber();
+        subscriber.setNumber(String.valueOf(id));
+
+        Conference conference = new Conference();
+        conference.setAdminPin(String.valueOf(System.currentTimeMillis()));
+        conference.setIsStarted(true);
+        conference.setId(System.currentTimeMillis());
+        conference.setNumber(subscriber.getNumber());
+
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        session.save(conference);
+        tx.commit();
+
+        User user = new User();
+        user.setSubscriber(subscriber);
+
+        HttpSession httpSession = request.getSession();
+        httpSession.setAttribute("user", user);
+
+        request.setMethod("GET");
+        servlet.service(request, response);
+
+        String number = "\"number\":\"" + conference.getNumber() + "\"";
+        assertTrue(response.getContentAsString().contains(number));
+    }
+}
