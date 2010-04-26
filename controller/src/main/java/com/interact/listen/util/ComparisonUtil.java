46c150fab549618c7684c6ef4684af83be26a4c0
rob <rob.hruska@newnet.com>
2010-04-26 21:27:09 +0000

    Added FindBugs suppression to ComparisonUtil.isEqual().
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@276 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/src/main/java/com/interact/listen/util/ComparisonUtil.java
+++ controller/src/main/java/com/interact/listen/util/ComparisonUtil.java
@@ -7,6 +7,7 @@ public final class ComparisonUtil
         throw new AssertionError("Cannot instantiate utility class ComparisonUtil");
     }
 
+    @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "NP_NULL_ON_SOME_PATH_MIGHT_BE_INFEASIBLE", justification = "object1 won't be null where FindBugs says it will, see the JUnit tests")
     public static boolean isEqual(Object object1, Object object2)
     {
         if(object1 == null && object2 == null)
