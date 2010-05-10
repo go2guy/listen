6713defacda6887fa96cac4de455411fb20f0f59
joe <joe.ebemier@newnet.com>
2010-05-10 13:25:42 +0000

    Updated self-extracting packages.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@370 4692729f-32a0-46b7-9832-1bf53c4881b5

--- release/pack.py
+++ release/pack.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 try:
-    import md5, base64, re
-    from optparse import OptionParser
+    import os, md5, base64, re
+    from optparse import OptionParser, TitledHelpFormatter
 
 except ImportError, e:
     raise "Unable to import required module: " + str(e)
 
 def build(name, files):
-    template = open("unpack.py", "r")
-    outfile = open(name, "w")
+    template = open('unpack.py', 'r')
+    outfile = open(name, 'w')
 
     # Write all of the template code into output file up to the "load" method.
     templines = template.readlines()
@@ -30,7 +30,7 @@ def build(name, files):
         for packline in packfile.readlines():
             contents.append(packline)
             encont.append(base64.b64encode(packline))
-        packfiles.append("packfile('%s','%s',['%s'])" % (packfile.name, md5.new("".join(contents)).hexdigest(), "','".join(encont)))
+        packfiles.append("packfile('%s','%s',['%s'])" % (os.path.basename(packfile.name), md5.new("".join(contents)).hexdigest(), "','".join(encont)))
 
     outfile.write("    global packfiles" + "\n")
     outfile.write("    packfiles = [%s]" % ",".join(packfiles) + "\n")
@@ -44,24 +44,14 @@ def build(name, files):
 def main():
     # Set up a parser object which defines how to parse the expected input
     parser = OptionParser()
-
-    parser.add_option(
-        "",
-        "--name",
-        dest="name",
-        action="store",
-        metavar="NAME",
-        default=None,
-        help="The name of the archive to be created.")
+    parser.usage = "%prog [options] FILE ..."
+    parser.description = "Creates a self-extracting archive from all files passed in on the command line."
+    parser.formatter = TitledHelpFormatter(indent_increment=2, max_help_position=30, width=90)
 
     # Parse input parameters
     (opts, args) = parser.parse_args()
 
-    # system is required
-    if opts.name == None:
-        parser.error("--name option not supplied")
-
-    build(opts.name, args)
+    build('listen', args)
 
 if __name__ == "__main__":
    main()
--- release/unpack.py
+++ release/unpack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 try:
-    import os, md5, base64
-    from optparse import OptionParser
+    import sys, os, re, md5, base64, datetime
+    from optparse import OptionParser, TitledHelpFormatter
 
 except ImportError, e:
     raise "Unable to import required module: " + str(e)
@@ -35,13 +35,70 @@ class packfile(object):
 def load():
 
 
-def default():
-    print("Installing using defaults.")
+def default(unpackdir):
+    # Should be 3 packs (uia, listen, and Realize)
+    uiapkg = None
+    listenpkg = None
+    realizepkg = None
+
+    reuiapkg = re.compile("^uia-")
+    relistenpkg = re.compile("^listen-")
+    rerealizepkg = re.compile("^Reailze-")
+    for unpackfile in packfiles:
+        if reuiapkg.match(unpackfile.name) != None:
+            uiapkg = unpackfile
+
+        if relistenpkg.match(unpackfile.name) != None:
+            listenpkg = unpackfile
+
+        if rerealizepkg.match(unpackfile.name) != None:
+            realizepkg = unpackfile
+
+    if uiapkg == None or listenpkg == None or realizepkg == None:
+        sys.exit("Unable to find required packages in bundle.")
+
+    run(["rpm", "-Uvh", unpackdir + "/" + uiapkg])
+    run(["/interact/packages/iiInstall.sh", "-i", "--noinput", "--replacefiles", "--replacepkgs", unpackdir + "/" + listenpkg, "all"])
+    run(["/interact/packages/iiInstall.sh", "-i", "--noinput", "--replacefiles", "--replacepkgs", unpackdir + "/" + realizepkg, "all"])
+
+
+def start():
+    # Start all associated processes
+    run("/interact/master/iiMoap")
+    run("/interact/master/iiSysSrvr")
+    run("service", "collector", "start")
+    run("Service", "listen-controller", "start")
+    run("service", "statistics", "start")
+    run("service", "tomcat", "start")
+
+
+def run(command, shell=False, failonerror=True):
+    print("Executing command [ %s ]." % " ".join(command))
+
+    try:
+        start = datetime.datetime.now()
+        retval = subprocess.call(command, shell=shell)
+        end = datetime.datetime.now()
+        print("Execution took [ %s ]." % str(end - start))
+
+        if retval != 0:
+            print("Execution of [ %s ] failed (returned [ %s ])." % (" ".join(command), str(retval)))
+            if failonerror:
+                sys.exit(1)
+
+    except Exception, e:
+        print("Execution of [ %s ] failed (returned [ %s ])." % (" ".join(command), str(e)))
+        if failonerror:
+            sys.exit(1)
 
 
 def main():
-    # Set up a parser object which defines how to parse the expected input
+    unpackdir = "/interact/packages"
+
+    # Set up a parser object which defines how to parse the expected input.
     parser = OptionParser()
+    parser.description = """This package is a self-extracting listen bundle. Execute this file to extract all packages to the %s directory. Specify the "default" directive to install the extracted packages using all defaults.""" % unpackdir
+    parser.formatter = TitledHelpFormatter(indent_increment=2, max_help_position=30, width=90)
 
     parser.add_option(
         "-d",
@@ -51,19 +108,42 @@ def main():
         default=False,
         help="Install using all default values.")
 
-    # Parse input parameters
+    parser.add_option(
+        "-s",
+        "--start",
+        dest="start",
+        action="store_true",
+        default=False,
+        help="Start all associated processes when installation is finished. (only applicable if \"default\" has also been specified.")
+
+    # Parse input parameters.
     (opts, args) = parser.parse_args()
 
-    unpackdir = "./interact/packages/"
+    # Create unpack directory if necessary."
     if not os.path.exists(unpackdir):
         os.makedirs(unpackdir)
 
+    # Load packages into memory.
+    print("Loading package to memory...")
+    start = datetime.datetime.now()
     load()
+    end = datetime.datetime.now()
+    print("Loading package to memory took [ %s ]." % str(end - start))
+
+    # Write files to disk.
+    print("Unpacking bundle into [ %s ]..." % unpackdir)
+    start = datetime.datetime.now()
     for unpackfile in packfiles:
         unpackfile.unpack(unpackdir)
+    end = datetime.datetime.now()
+    print("Unpacking bundle took [ %s ]." % str(end - start))
 
+    # If default was specified, run default commands
     if opts.default:
-        default()
+        default(unpackdir)
+
+        if opts.start:
+            start()
 
 if __name__ == "__main__":
    main()
