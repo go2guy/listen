02d48132076799037a6106e92b36cf001897ebb9
rob <rob.hruska@newnet.com>
2010-05-21 21:46:11 +0000

    Added Listen stats for Get DNIS meta API.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@481 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/src/main/java/com/interact/listen/api/GetDnisServlet.java
+++ controller/src/main/java/com/interact/listen/api/GetDnisServlet.java
@@ -3,6 +3,9 @@ package com.interact.listen.api;
 import com.interact.listen.ServletUtil;
 import com.interact.listen.config.Configuration;
 import com.interact.listen.config.Property;
+import com.interact.listen.stats.InsaStatSender;
+import com.interact.listen.stats.Stat;
+import com.interact.listen.stats.StatSender;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +21,13 @@ public class GetDnisServlet extends HttpServlet
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     {
+        StatSender statSender = (StatSender)request.getSession().getServletContext().getAttribute("statSender");
+        if(statSender == null)
+        {
+            statSender = new InsaStatSender();
+        }
+        statSender.send(Stat.META_API_GET_DNIS);
+
         String number = request.getParameter("number");
         if(number == null || number.trim().equals(""))
         {
