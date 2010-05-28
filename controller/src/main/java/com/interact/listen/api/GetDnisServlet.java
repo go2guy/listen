416954a1f924feb3029b732e7661ed4425e1a32a
rob <rob.hruska@newnet.com>
2010-05-28 19:42:03 +0000

    getDnis API now supports wildcards. A DNIS mapping can only contain one wildcard character, and it must be at the end of the mapping string. These criteria are not yet enforced on the screens.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@531 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/src/main/java/com/interact/listen/api/GetDnisServlet.java
+++ controller/src/main/java/com/interact/listen/api/GetDnisServlet.java
@@ -8,8 +8,7 @@ import com.interact.listen.stats.InsaStatSender;
 import com.interact.listen.stats.Stat;
 import com.interact.listen.stats.StatSender;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -33,19 +32,69 @@ public class GetDnisServlet extends HttpServlet
         String number = request.getParameter("number");
         if(number == null || number.trim().equals(""))
         {
-            throw new ListenServletException(HttpServletResponse.SC_BAD_REQUEST, "Please provide a number", "text/plain");
+            throw new ListenServletException(HttpServletResponse.SC_BAD_REQUEST, "Please provide a number",
+                                             "text/plain");
         }
 
         String configuration = Configuration.get(Property.Key.DNIS_MAPPING);
         Map<String, String> mappings = dnisConfigurationToMap(configuration);
-        if(mappings.containsKey(number))
+        String mapping = findMapping(number, mappings);
+        if(mapping == null)
         {
-            response.setStatus(HttpServletResponse.SC_OK);
-            OutputBufferFilter.append(request, mappings.get(number), "text/plain");
-            return;
+            throw new ListenServletException(HttpServletResponse.SC_NOT_FOUND);
         }
 
-        throw new ListenServletException(HttpServletResponse.SC_NOT_FOUND);
+        response.setStatus(HttpServletResponse.SC_OK);
+        OutputBufferFilter.append(request, mappings.get(number), "text/plain");
+    }
+
+    private String findMapping(String number, Map<String, String> mappings)
+    {
+        // if we have a specific mapping for the number, simply return it
+        if(mappings.containsKey(number))
+        {
+            return mappings.get(number);
+        }
+
+        // TreeMap, ordered by the length of its entries, descending - this sorting will allow us to loop to find a
+        // specific match later
+        Map<String, String> wildcards = new TreeMap<String, String>(new Comparator<String>()
+        {
+            @Override
+            public int compare(String a, String b)
+            {
+                return Integer.valueOf(b.length()).compareTo(a.length());
+            }
+        });
+
+        // strip all of the specific mappings (we didn't find one at this point, so we're looking for a wildcarded one)
+        // also take out any mappings that are longer than the number itself, they won't match
+        for(Map.Entry<String, String> entry : mappings.entrySet())
+        {
+            if(entry.getKey().endsWith("*") && entry.getKey().length() < number.length() + 1)
+            {
+                wildcards.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        for(Map.Entry<String, String> entry : wildcards.entrySet())
+        {
+            // length of 1 has to be a single '*'; if we got this far, we matched the '*'
+            if(entry.getKey().length() == 1)
+            {
+                return entry.getValue();
+            }
+
+            // if the first M (m = key.length) digits of number.length equal the key, we found a match
+            int keyLength = entry.getKey().length();
+            if(entry.getKey().substring(0, keyLength - 2).equals(number.substring(0, keyLength - 2)))
+            {
+                return entry.getValue();
+            }
+        }
+
+        // no match found
+        return null;
     }
 
     private Map<String, String> dnisConfigurationToMap(String configurationValue)
