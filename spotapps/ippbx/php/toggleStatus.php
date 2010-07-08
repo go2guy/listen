c38cf5c34bc5d890c299a7e65efb4f9687627749
ladi <ladi.ak@newnet.com>
2010-07-08 13:38:01 +0000

    Updated logic used in php script
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@803 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/ippbx/php/toggleStatus.php
+++ spotapps/ippbx/php/toggleStatus.php
@@ -1,26 +1,42 @@
 <?php
+# Set Global Variables
+$objName = "clientToggle";
+$result  = "Failure";
+$reason  = "";
 
 # Grab inputs
 $t_ext      = @$_REQUEST['extension'];
 $t_status   = @$_REQUEST['status'];
 
-# Connect to db
-$t_connect = mysql_connect("localhost","root","super");
-if (!$t_connect) {
-    die('ERROR: Unable to connect - ' . mysql_error());
+# Connect to mysql
+$t_connect = mysql_connect("localhost","root");
+if (!$t_connect)
+    exitresult($objName, $result);
+
+# Select db
+if(!(mysql_select_db("ip_pbx", $t_connect)))
+    exitresult($objName, $result);
+
+# Run query
+$t_query = "UPDATE extension_map SET in_use=\"$t_status\" WHERE ext=\"$t_ext\"";
+mysql_query($t_query);
+
+# Check result
+if ((mysql_affected_rows($t_connect)) != 1)
+    exitresult($objName, $result);
+else {
+    $result = "Success";
+    exitresult($objName,$result);
 }
 
-mysql_select_db("ip_pbx", $t_connect);
-$t_query = "UPDATE extension_map SET in_use=$t_status WHERE ext=$t_ext";
-
-if (mysql_query($t_query)) {
+# Exit PHP
+function exitresult($objName, $result, $reason=""){
     echo "<?xml version=\"1.0\"?>\n";
-    echo "<clientToggle>\n";
-    echo "\t<result>Success</result>\n";
-    echo "</clientToggle>\n";
-} else {
-    echo "<?xml version=\"1.0\"?>\n";
-    echo "<clientToggle>\n";
-    echo "\t<result>Failure</result>\n";
-    echo "</clientToggle>\n";
+    echo "<$objName>\n";
+    echo "\t<result>$result</result>\n";
+    if ($result == "Failure")
+        $reason=mysql_error();
+    echo "\t<reason>$reason</reason>\n";
+    echo "</$objName>\n";
+    exit;
 }
