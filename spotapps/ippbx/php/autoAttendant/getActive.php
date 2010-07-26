4ca70ea2cc0bb7d14ac392845d705aeb59e102d8
ladi <ladi.ak@newnet.com>
2010-07-26 14:50:20 +0000

    Added date validation logic
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@897 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/ippbx/php/autoAttendant/getActive.php
+++ spotapps/ippbx/php/autoAttendant/getActive.php
@@ -2,7 +2,7 @@
 require_once('db.php');
 # get list of available prompts
 $t_now = time();
-$t_sql = "SELECT name FROM available_prompts p, active_prompts a WHERE p.id=a.available_prompts_id AND '$t_now' BETWEEN start AND end";
+$t_sql = "SELECT name FROM available_prompts p, active_prompts a WHERE p.id=a.available_prompts_id AND '$t_now' BETWEEN start AND end ORDER BY start DESC LIMIT 0, 1";
 $t_result = mysql_query( $t_sql );
 $t_available_count = 0; 
 $t_prompt_name = 0;
