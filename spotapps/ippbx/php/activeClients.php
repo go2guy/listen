7634747000c3dec55bb516358d0450ac8a57942a
ladi <ladi.ak@newnet.com>
2010-08-02 19:03:36 +0000

    Added logic for call pick up (from hold)
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@943 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/ippbx/php/activeClients.php
+++ spotapps/ippbx/php/activeClients.php
@@ -8,6 +8,7 @@ $reason  = "";
 $t_flag     = @$_REQUEST['flag'];
 $t_ext      = @$_REQUEST['extension'];
 $t_id       = @$_REQUEST['id'];
+$t_sid      = @$_REQUEST['sessionID'];
 
 # Connect to mysql
 $t_connect = mysql_connect("localhost","root");
@@ -23,19 +24,33 @@ if ($t_flag == 'insert')
     $t_update = "INSERT INTO active_clients (connection_id, client) VALUES (\"$t_id\", \"$t_ext\")";
 else if ($t_flag == 'delete')
     $t_update = "DELETE FROM active_clients WHERE connection_id=\"$t_id\" AND client=\"$t_ext\" LIMIT 1";
+else if ($t_flag == 'update')
+    $t_update = "UPDATE active_clients SET session_id=\"$t_sid\" WHERE connection_id=\"$t_id\" AND client=\"$t_ext\"";
+else if ($t_flag == 'select')
+    $t_update = "SELECT session_id FROM active_clients WHERE client=\"$t_ext\"";
 else
     exitresult($objName, $result);
 
 # Run insert/delete
-mysql_query($t_update);
+$t_val = mysql_query($t_update);
 
 # Check result
-if ((mysql_affected_rows($t_connect)) != 1)
-    exitresult($objName, $result);
-else {
-    $result = "Success";
-    exitresult($objName,$result);
+if ($t_flag == 'select') {
+    if ((mysql_num_rows ($t_val)) === 1) {
+        $t_row = mysql_fetch_array ($t_val);
+        $result = $t_row['session_id'];
+    }
+    else {
+        # Nothing to do here. Just exit with result == 'Faliure'
+    }
 }
+else if ((mysql_affected_rows($t_connect)) === 1)
+    $result = "Success";
+else {
+    # Nothing to do here. Just exit with result == 'Faliure'
+}
+
+exitresult($objName, $result);
 
 # Exit PHP
 function exitresult($objName, $result, $reason=""){
--- spotapps/ippbx/sql/ippbx_schema.sql
+++ spotapps/ippbx/sql/ippbx_schema.sql
@@ -1,14 +1,11 @@
-create database ip_pbx;
-use ip_pbx;
-
 -- phpMyAdmin SQL Dump
--- version 3.3.2deb1
+-- version 2.11.10
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 15, 2010 at 12:13 PM
--- Server version: 5.1.41
--- PHP Version: 5.3.2-1ubuntu4.2
+-- Generation Time: Aug 02, 2010 at 09:39 AM
+-- Server version: 5.0.22
+-- PHP Version: 5.1.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -27,7 +24,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `active_clients` (
   `connection_id` varchar(255) NOT NULL,
-  `client` varchar(50) NOT NULL
+  `client` varchar(50) NOT NULL,
+  `session_id` varchar(255) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -41,14 +39,14 @@ CREATE TABLE IF NOT EXISTS `active_clients` (
 --
 
 CREATE TABLE IF NOT EXISTS `active_prompts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `start` int(11) NOT NULL,
   `end` int(11) NOT NULL,
   `available_prompts_id` int(255) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `start` (`start`),
   UNIQUE KEY `end` (`end`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `active_prompts`
@@ -62,26 +60,41 @@ CREATE TABLE IF NOT EXISTS `active_prompts` (
 --
 
 CREATE TABLE IF NOT EXISTS `available_prompts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `available_prompts`
 --
 
 INSERT INTO `available_prompts` (`id`, `name`, `description`) VALUES
-(1, '1401', 'Thanks for calling Interact Incorporated, we are currently closed to welcome in the New Year'),
-(2, '1405', 'Thanks for calling Interact Incorporated, we are currently closed for the Memorial Day Holiday'),
-(3, '1407', 'Thanks for calling Interact Incorporated, we are currently closed in observance of the Independence Day Holiday'),
-(4, '1409', 'Thanks for calling Interact Incorporated, we are currently closed for the Labor Day Holiday'),
-(5, '1411', 'Thanks for calling Interact Incorporated, we are currently closed for the Thanksgiving holiday'),
-(6, '1412', 'Thanks for calling Interact Incorporated, we are currently closed for our year end meeting and Holiday party'),
-(7, '1413', 'Thanks for calling Interact Incorporated, we are currently closed for the Christmas holiday'),
-(8, '1414', 'Thank you for calling Interact Inc. We are currently closed for the Holidays'),
-(9, '1415', 'Thank you for calling Interact Inc. We are currently closed for the Holiday'),
-(10, '1416', 'Thanks for calling Interact Incorporated, we are currently closed for our year end meeting'),
-(11, '1417', 'Thanks for calling Interact Incorporated, we are currently due to the inclement weather');
+(1, '1401', 'Thank you for calling Interact Incorporated. We are currently closed to welcome in the New Year'),
+(2, '1405', 'Thank you for calling Interact Incorporated. We are currently closed for the Memorial Day Holiday'),
+(3, '1407', 'Thank you for calling Interact Incorporated. We are currently closed in observance of the Independence Day Holiday'),
+(4, '1409', 'Thank you for calling Interact Incorporated. We are currently closed for the Labor Day Holiday'),
+(5, '1411', 'Thank you for calling Interact Incorporated. We are currently closed for the Thanksgiving holiday'),
+(8, '1414', 'Thank you for calling Interact Incorporated. We are currently closed for the Holidays'),
+(9, '1415', 'Thank you for calling Interact Incorporated. We are currently closed for the Holiday'),
+(11, '1417', 'Thank you for calling Interact Incorporated. We are currently closed due to the inclement weather'),
+(12, '1402', 'Thank you for calling Interact Incorporated. We are currently closed for a company event'),
+(13, '1403', 'Thank you for calling Interact Incorporated. Our office is currently closed');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forwarded_extensions`
+--
+
+CREATE TABLE IF NOT EXISTS `forwarded_extensions` (
+  `origination` int(11) NOT NULL,
+  `destination` varchar(25) NOT NULL,
+  `time_stamp` int(11) NOT NULL,
+  PRIMARY KEY  (`origination`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `forwarded_extensions`
+--
