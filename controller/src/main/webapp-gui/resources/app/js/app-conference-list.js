f365fef4d01738d492b6d3dd782b808caf969ff3
rob <rob.hruska@newnet.com>
2010-08-04 22:10:03 +0000

    Conference list javascript application is now namespaced.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@976 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/src/main/webapp-gui/resources/app/js/app-conference-list.js
+++ controller/src/main/webapp-gui/resources/app/js/app-conference-list.js
@@ -1,47 +1,52 @@
 $(document).ready(function() {
-    Listen.registerApp(new Listen.Application('conference-list', 'conference-list-application', 'menu-conference-list', new ConferenceList()));
-});
 
-function ConferenceList() {
-    var interval;
+    Listen.Conferences = function() {
+        return {
+            Application: function() {
+                Listen.trace('Listen.Conferences.Application [construct]');
+                var interval;
+                var dynamicTable = new Listen.DynamicTable({
+                    url: '/ajax/getConferenceList',
+                    tableId: 'conference-list-table',
+                    templateId: 'conference-row-template',
+                    retrieveList: function(data) {
+                        return data.results;
+                    },
+                    paginationId: 'conference-list-pagination',
+                    updateRowCallback: function(row, data, animate) {
+                        Listen.setFieldContent(row.find('.conference-cell-description'), data.description, animate);
+                        var status = data.isStarted ? 'Started' : 'Not Started';
+                        if(data.isStarted) {
+                            status += ' ' + data.startDate;
+                        }
+                        Listen.setFieldContent(row.find('.conference-cell-status'), status, animate);
+                        Listen.setFieldContent(row.find('.conference-cell-callerCount'), data.callerCount, animate);
+                        Listen.setFieldContent(row.find('.conference-cell-view'), '<button class="button-view" onclick="Listen.Conferences.viewConference(' + data.id + ');">View</button>', false, true);
+                    }
+                });
 
-    var dynamicTable = new Listen.DynamicTable({
-        url: '/ajax/getConferenceList',
-        tableId: 'conference-list-table',
-        templateId: 'conference-row-template',
-        retrieveList: function(data) {
-            return data.results;
-        },
-        paginationId: 'conference-list-pagination',
-        updateRowCallback: function(row, data, animate) {
-            Listen.setFieldContent(row.find('.conference-cell-description'), data.description, animate);
-            var status = data.isStarted ? 'Started' : 'Not Started';
-            if(data.isStarted) {
-                status += ' ' + data.startDate;
+                this.load = function() {
+                    Listen.trace('Loading conferences');
+                    dynamicTable.pollAndSet(false);
+                    interval = setInterval(function() {
+                        dynamicTable.pollAndSet(true);
+                    }, 1000);
+                };
+
+                this.unload = function() {
+                    Listen.trace('Unloading conferences');
+                    if(interval) {
+                        clearInterval(interval);
+                    }
+                };
+            },
+
+            viewConference: function(id) {
+                var conference = new Conference(id);
+                Listen.switchApp('conferencing', conference);
             }
-            Listen.setFieldContent(row.find('.conference-cell-status'), status, animate);
-            Listen.setFieldContent(row.find('.conference-cell-callerCount'), data.callerCount, animate);
-            Listen.setFieldContent(row.find('.conference-cell-view'), '<button class="button-view" onclick="viewConference(' + data.id + ');">View</button>', false, true);
         }
-    });
+    }();
 
-    this.load = function() {
-        Listen.trace('Loading conference list');
-        dynamicTable.pollAndSet(false);
-        interval = setInterval(function() {
-            dynamicTable.pollAndSet(true);
-        }, 1000);
-    };
-
-    this.unload = function() {
-        Listen.trace('Unloading conference-list');
-        if(interval) {
-            clearInterval(interval);
-        }
-    };
-}
-
-function viewConference(id) {
-    var conference = new Conference(id);
-    Listen.switchApp('conferencing', conference);
-}
+    Listen.registerApp(new Listen.Application('conference-list', 'conference-list-application', 'menu-conference-list', new Listen.Conferences.Application()));
+});
