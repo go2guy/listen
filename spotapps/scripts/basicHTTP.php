21a54aac13e8e0b69e72be99db28dbfeaaa2878c
ladi <ladi.ak@newnet.com>
2010-08-19 16:01:07 +0000

    Fixed TP 16038
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1062 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/scripts/basicHTTP.php
+++ spotapps/scripts/basicHTTP.php
@@ -5,16 +5,17 @@
 
     #Grab inputs
     $sessionID  = @$_REQUEST['sessionID']   or exitresult ($objName,$result,"'sessionID' argument missing from request");
-    $argList    = @$_REQUEST['argList']     or exitresult ($objName,$result,"'argList' argument missing from request");
+    $objStr     = @$_REQUEST['argList']     or exitresult ($objName,$result,"'argList' argument missing from request");
+    $str = urlencode ($objStr);
 
-    $httpRequest  = "curl http://localhost/spot/ccxml/basichttp -d sessionid=$sessionID -d II_SB_basichttpEvent=\"CREATESESSION\" -d II_SB_argument=$argList";
+    $httpRequest  = "curl http://localhost/spot/ccxml/basichttp -d sessionid=$sessionID -d name=\"dialog.user.basichttp\" -d II_SB_argument=\"$str\" -d II_SB_basichttpEvent=\"CREATESESSION\" -d II_SB_URI=\"listen_main/listen_main.ccxml\"";
 
     // Execute http request
     $result = exec($httpRequest);
     if ($result == "")
         $result = "Success";
     exitresult ($objName, $result, "");
-    
+
     #Exit php
     function exitresult ($objname, $result, $reason="") {
         echo "<?xml version=\"1.0\"?>\n";
