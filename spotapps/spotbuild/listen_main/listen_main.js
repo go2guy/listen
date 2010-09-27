aee7a90649424d3932d49e34b2283713a9f1bc45
ladi <ladi.ak@newnet.com>
2010-09-27 16:07:20 +0000

    Updated files for unit testing
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1193 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ spotapps/spotbuild/listen_main/listen_main.js
@@ -0,0 +1,66 @@
+function getAppName(passValues) {
+    var result = '';
+    var tmpVAl = new String(passValues).replace(/^\s*|\s*$|\s+/g,'');
+    if((tmpVAl == '') || (typeof(passValues) == 'undefined'))
+        result = "DEFAULT"; 
+    else {
+        var tmpVal = eval("("+passValues+")");
+        switch (tmpVal.application) {
+            case "AUTO_DIAL":
+                result = "listen_autoDial";
+                break;
+            case "RECORD":
+                result = "listen_record";
+                break;
+            case "DEL_ARTIFACT":
+                result = "listen_artifacts";
+                break;
+            case "CONF_EVENT":
+                result = "listen_confEvents";
+                break;
+            case "MSG_LIGHT":
+                result = "msgLightCntrl";
+                break;
+            case "conferencing":
+                result = "listen_conference";
+                break;
+            case "mailbox":
+                result = "listen_mailbox";
+                break;
+            case "directVoicemail":
+                result = "listen_mailbox";
+                break;
+            case "voicemail":
+                result = "listen_voicemail";
+                break;
+            default:
+                result = tmpVal.application;
+        }
+    }
+    return result;
+}
+
+function extendAppObject(passValues,cntrlURL,hostName,sysAccessTime,HTTPcontroller,sipURL,STATcontroller,artifactsDIR) {
+    var result = eval("("+passValues+")");
+    result.cntrlURL = cntrlURL + "/api";
+    result.hostName = hostName;
+    result.sipURL = sipURL;
+    result.sysAccessTime = sysAccessTime;
+    result.HTTPcontroller = HTTPcontroller;
+    result.STATcontroller = STATcontroller;
+    result.artifactsDIR = artifactsDIR;
+    return iiStringify(result);
+}
+
+function createHostObject(hostName,DNIS,callType) {
+    return "{\"httpApi\": \"http://"+hostName+"/spot\", \"phoneNumber\":\""+DNIS+"\", \"phoneNumberProtocol\": \""+callType+"\" }";
+}
+
+function setDnisSearchSting(DNIS) {
+    var subID = getnum(DNIS);
+    return "getDnis?number=" + escape(subID);
+}
+
+function createAppObject(appToAccess,sysAccessTime,sipURL,cntrlURL,hostName,HTTPcontroller,artifactsDIR,STATcontroller) {
+    return "{\"application\":\""+appToAccess+"\", \"sysAccessTime\": \""+ sysAccessTime+"\", \"sipURL\":\""+sipURL+"\", \"cntrlURL\":\""+cntrlURL+"/api\", \"hostName\":\""+hostName+"\", \"HTTPcontroller\": \""+ HTTPcontroller+"\", \"artifactsDIR\": \""+ artifactsDIR+"\", \"STATcontroller\": \""+ STATcontroller+"\"}";
+}
