61b7cfd14cabcc18349bd7dbc1353dd2e86d69a4
ladi <ladi.ak@newnet.com>
2010-09-27 15:59:47 +0000

    Rhinounit framework
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1192 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ spotapps/spotbuild/rhinounit/test/listen_main/getAppNameTest.js
@@ -0,0 +1,66 @@
+/*
+NAME: getAppNameTest.js
+DESC: This file contains test cases for the getAppName function.
+      It loads listen_main.js (which contains getAppName). It
+      also defines relevant test cases for this function.
+*/
+
+eval(loadFile("../listen_main/listen_main.js"));    //Load lib file containing function to be tested
+
+// Define variables & object used for testing
+var appObjIsUndefined;
+var testVal = 'testAppName';
+var emptyObj = '';
+var justSpace = '  ';
+var setAppObj = {
+    appName: function (name) {
+        var currTime = new Date().getTime();
+        return '{"application":"' + name + '", "sysAccessTime": "' + currTime + '", "sipURL":"testServer", "cntrlURL":"http://localhost:9091/api", "hostName":"localhost", "HTTPcontroller": "http://localhost/spot/cgi-bin/spotbuild/listen/controller.php", "artifactsDIR": "/interact/listen/artifacts/", "STATcontroller": "http://localhost:9413/collect/stat"}';
+    } // This object contains one member whose value is a function.
+};
+
+//Here are the test cases
+testCases(test,
+    function shouldReturnDefaultAsResult1() {
+        assert.that(getAppName(appObjIsUndefined), eq("DEFAULT"));
+    },
+    function shouldReturnDefaultAsResult2() {
+        assert.that(getAppName(emptyObj), eq("DEFAULT"));
+    },
+    function shouldReturnDefaultAsResult3() {
+        assert.that(getAppName(justSpace), eq("DEFAULT"));
+    },
+    function shouldReturnAnEmptyStringResult() {
+        assert.that(getAppName(setAppObj.appName('')), eq(""));
+    },
+    function shouldReturnTestAppNameAsResult() {
+        assert.that(getAppName(setAppObj.appName(testVal)), eq("testAppName"));
+    },
+    function shouldReturnListen_autoDialAsResult() {
+        assert.that(getAppName(setAppObj.appName('AUTO_DIAL')), eq("listen_autoDial"));
+    },
+    function shouldReturnListen_recordAsResult() {
+        assert.that(getAppName(setAppObj.appName('RECORD')), eq("listen_record"));
+    },
+    function shouldReturnListen_artifactsAsResult() {
+        assert.that(getAppName(setAppObj.appName('DEL_ARTIFACT')), eq("listen_artifacts"));
+    },
+    function shouldReturnListen_confEventsAsResult() {
+        assert.that(getAppName(setAppObj.appName('CONF_EVENT')), eq("listen_confEvents"));
+    },
+    function shouldReturnMsgLightCntrlAsResult() {
+        assert.that(getAppName(setAppObj.appName('MSG_LIGHT')), eq("msgLightCntrl"));
+    },
+    function shouldReturnConferencingAsResult() {
+        assert.that(getAppName(setAppObj.appName('conferencing')), eq("listen_conference"));
+    },
+    function shouldReturnListen_mailboxAsResult1() {
+        assert.that(getAppName(setAppObj.appName('mailbox')), eq("listen_mailbox"));
+    },
+    function shouldReturnListen_mailboxAsResult2() {
+        assert.that(getAppName(setAppObj.appName('directVoicemail')), eq("listen_mailbox"));
+    },
+    function shouldReturnListen_voicemailAsResult() {
+        assert.that(getAppName(setAppObj.appName('voicemail')), eq("listen_voicemail"));
+    }
+);
