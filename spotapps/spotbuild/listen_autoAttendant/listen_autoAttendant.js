a46c32f619a7c274bf746609e848cb9e9130419a
ladi <ladi.ak@newnet.com>
2010-10-13 16:28:50 +0000

    Added Javascript lib file to listen_autoAttendant
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1253 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ spotapps/spotbuild/listen_autoAttendant/listen_autoAttendant.js
@@ -0,0 +1,13 @@
+function setControllerURL(appObj) {
+    var tmpVal = eval("("+appObj+")");
+    var result = tmpVal.cntrlURL.split('/api');
+    return result[0] + "/meta";
+}
+
+function getTimeDifference(startTime, endTime) {
+    var num = /^\d+$/;
+    if ((num.test(startTime)) && (num.test(endTime)))
+        return Math.round((endTime - startTime)/1000);
+    else
+        return 1;
+}
