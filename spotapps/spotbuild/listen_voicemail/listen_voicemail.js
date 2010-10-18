d89ebff571858c4d1717cd9560d32ab85be1c1ac
ladi <ladi.ak@newnet.com>
2010-10-18 17:07:31 +0000

    Updated Javascript functions
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1287 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ spotapps/spotbuild/listen_voicemail/listen_voicemail.js
@@ -0,0 +1,42 @@
+function getResultsKeyValue(jsonObj, index, key) {
+    var result;
+    var tmpVal = eval("("+jsonObj+")");
+    result = tmpVal.results[index][key];
+    if (result == null)
+        result = "";
+    return result;
+}
+
+function getAccessNumSubID(jsonObj, status) {
+    var result = "NOT_FOUND";
+    var tmpVal = eval("("+jsonObj+")");
+    if ((status == 'Success') && (tmpVal.count== 1))
+        result = getNextElement('L',tmpVal.results[0].subscriber.href,'/');
+    return result;
+}
+
+function setCallerName (jsonObj, status, ANI) {
+    var result = getnum(ANI);
+    if (status == 'Success') {
+        var tmpVal = getJsonVal(jsonObj, 'realName');
+        if (tmpVal.length > 0)
+            result = tmpVal;
+    }
+    return result;
+}
+
+function createTranscriptionObj(returnVal, fileLocation, passValues) {
+    var number = eval("("+returnVal+")");
+    var result = eval("("+passValues+")");
+    result.fileLocation = fileLocation;
+    result.fileNumber = number.id;
+    return "\""+iiStringify(result)+"\"";
+}
+
+function getTimeDifference(startTime, endTime) {
+    var num = /^\d+$/;
+    if ((num.test(startTime)) && (num.test(endTime)))
+        return Math.round((endTime - startTime)/1000);
+    else
+        return 1;
+}
