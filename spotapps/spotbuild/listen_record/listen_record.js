054d9ce671c385d7f04768d1c5addb019cf909d3
ladi <ladi.ak@newnet.com>
2010-10-20 16:14:36 +0000

    Updated Javascript lib file
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1311 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/spotbuild/listen_record/listen_record.js
+++ spotapps/spotbuild/listen_record/listen_record.js
@@ -10,29 +10,10 @@ function getArcadeId(argList) {
     return result;
 }
 
-function getSubscriber(returnVal) {
-    var result = eval("("+returnVal+")");
-    result = result.subscriber.href;
-    return getNextElement('L',result,'/');
-}
-
-function setRecFileName(argList, recPath, id ) {
-    var result = eval("("+argList+")");
-    return recPath + id + "-" + result.startTime + "-" + getTimeStamp(1) + ".wav";
-}
-
 function getChannelInfo(argList, key) {
     var result = eval("("+argList+")");
     if (result[key]== 'TUI') 
         return result.adminSID;
     else 
         return "GUI";
-}
-
-function createConfObject(id,isRecording,isStarted,recSID,arcadeID,confDescription) {
-    return "{\"href\": \"/conferences/"+id+"\", \"id\":"+id+", \"description\":\""+confDescription+"\", \"arcadeId\":\""+arcadeID+"\",\"recordingSessionId\":\""+recSID+"\", \"isRecording\":"+isRecording+", \"isStarted\": "+isStarted+"}";
-}
-
-function createConfRecObject(id,hostName,recSize,recFile,recLength,recInfo) {
-    return "{\"conference\": {\"href\": \"/conferences/"+id+"\"}, \"uri\":\"http://"+hostName+recFile+"\", \"description\":\""+recInfo+"\", \"duration\":\""+recLength+"\", \"fileSize\":\""+recSize+"\"}";
 }
