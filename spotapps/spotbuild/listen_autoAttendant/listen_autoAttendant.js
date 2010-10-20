88cc3d1786a377cc51493dad0261da7d141f1fbf
ladi <ladi.ak@newnet.com>
2010-10-20 15:41:38 +0000

    Updated Javascript lib file
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1306 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/spotbuild/listen_autoAttendant/listen_autoAttendant.js
+++ spotapps/spotbuild/listen_autoAttendant/listen_autoAttendant.js
@@ -3,11 +3,3 @@ function setControllerURL(appObj) {
     var result = tmpVal.cntrlURL.split('/api');
     return result[0] + "/meta";
 }
-
-function getTimeDifference(startTime, endTime) {
-    var num = /^\d+$/;
-    if ((num.test(startTime)) && (num.test(endTime)))
-        return Math.round((endTime - startTime)/1000);
-    else
-        return 1;
-}
