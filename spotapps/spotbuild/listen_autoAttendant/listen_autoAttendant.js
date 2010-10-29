e4d9dbf21955543d888f8853f208105318ebc493
ladi <ladi.ak@newnet.com>
2010-10-29 18:57:19 +0000

    Updated with new audio input node
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1339 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/spotbuild/listen_autoAttendant/listen_autoAttendant.js
+++ spotapps/spotbuild/listen_autoAttendant/listen_autoAttendant.js
@@ -3,3 +3,13 @@ function setControllerURL(appObj) {
     var result = tmpVal.cntrlURL.split('/api');
     return result[0] + "/meta";
 }
+
+function getMenuHotKeys(menuObj) {
+    var result = '';
+    var tmpVal = getJsonVal(menuObj, 'args');
+    for (var i=0; i<tmpVal.keyPresses.length; i++) {
+        if (tmpVal.keyPresses[i].length == 1)
+            result = result + tmpVal.keyPresses[i] + ',';
+    }
+    return result;
+}
