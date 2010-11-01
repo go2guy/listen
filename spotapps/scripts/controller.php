4c9cd72820eb1053ad29dfbddc387e6abd58b7d1
rob <rob.hruska@newnet.com>
2010-11-01 18:51:50 +0000

    Added Date and X-Listen-Signature headers to requests send to the controller.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1351 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/scripts/controller.php
+++ spotapps/scripts/controller.php
@@ -18,13 +18,16 @@
     $lstnChannel= @$_REQUEST['lstnChannel'] or $lstnChannel="";
     $lstnSub    = @$_REQUEST['lstnSub']     or $lstnSub="";
 
+    $date = date('D, d M Y H:i:s T');
+    $signature = sign($date);
+
     #Set up cURL options
     $curlHandle = curl_init();
     $method = strtoupper($method);
     switch ($method) {
         case 'GET':
             $destURL .= "/".$rsrc.$data;
-            $header = array("Accept: application/json", "X-Listen-AuthenticationType: U1lTVEVN");
+            $header = array("Accept: application/json", "X-Listen-AuthenticationType: U1lTVEVN", "Date: $date", "X-Listen-Signature: $signature");
             if ($lstnChannel != "") array_push($header, "X-Listen-Channel: ". $lstnChannel);
             if ($lstnSub != "") array_push($header, "X-Listen-Subscriber: ".  $lstnSub);
             $options = array(CURLOPT_URL => $destURL,
@@ -36,7 +39,7 @@
             break;
         case 'POST':
             $destURL .= "/".$rsrc;
-            $header = array("Content-Type: $ContentType", "X-Listen-AuthenticationType: U1lTVEVN");
+            $header = array("Content-Type: $ContentType", "X-Listen-AuthenticationType: U1lTVEVN", "Date: $date", "X-Listen-Signature: $signature");
             if ($lstnChannel != "") array_push($header, "X-Listen-Channel: ". $lstnChannel);
             if ($lstnSub != "") array_push($header, "X-Listen-Subscriber: ".  $lstnSub);
             $options = array(CURLOPT_URL => $destURL,
@@ -51,7 +54,7 @@
         case 'PUT':
         case 'DELETE':
             $destURL .= "/".$rsrc."/".(int)$ID;
-            $header = array("Content-Type: $ContentType", "Content-Length: ". strlen($data), "X-Listen-AuthenticationType: U1lTVEVN");
+            $header = array("Content-Type: $ContentType", "Content-Length: ". strlen($data), "X-Listen-AuthenticationType: U1lTVEVN", "Date: $date", "X-Listen-Signature: $signature");
             if ($lstnChannel != "") array_push($header, "X-Listen-Channel: ". $lstnChannel);
             if ($lstnSub != "") array_push($header, "X-Listen-Subscriber: ".  $lstnSub);
             $options = array(CURLOPT_URL => $destURL,
@@ -103,4 +106,12 @@
         echo "  <Reason>$reason</Reason>\n";
         echo "</{$objname}>\n";
         exit;
+    }
+
+    // create a request signature for the given date
+    function sign($date) {
+        $key = base64_decode('MIIEowIBAAKCAQEA5NmI8GIsupuvOMXR4yfs8hK2RUmX/CKlHmLEr/b1mPr/gx+fenafOSKoXs7OUvUxF/CR0EW6iUJvJ9lmTACTZFsTfF+mSJZ76dpuh6J8BQj9lpnH+AEp05LqLr2zvlzkksrjYSW4hfaZqfUZDk8YvGsdBqDpWrEykD16R5Jv5Iw/Y13Jd99F5zYU+Z3M+XdBSrFSaDwU5GeiOQVyl8q/Bt9gY7O1HfqYY9udXmAzPfEaZdCqCj7B8V8Sj7Wc92TZ/fHabZFKzfhVwfHCAzK4mQZ1be8snJd1f9R2peqjzBEINGdOnAnm0rGItKZJY91LMYi5H6Wh/Qh21CYY4Ne2wwIDAQABAoIBAEKXQg+goZ9TOfNtLJvKvFncNAmJVp5Zfm6PEuiZFfID52HCS+eYqNA5U4Dy8HqXOkfbCrLt90+Fc07HJcsrx7fGAK+KLZqlnzz3AH6bOzdD3HZ8HQH/ZKpZ76bWMH1ODnzgaLWWAlGI5kHcPgQ549q/2FxbakunkC0ElpZI+CIqWEf0zNfTXOpExMWx+FENntk/qpHijE+zcbh1/cy8Bsmj0WcXZ3aTDElG3XCC21rPd7zgrFeL6Sy26Br0eqxqddatghZHB5PhZYE4BYM3AlZTZVfuDHogXn4BXILTyV+oQHNUzHKjStfVryJnslnWVAF5whaXGm1fyRY3WBAEmuECgYEA+AivBccqnKfgB7BTDTUMCdt7v+vVbR7CEZc5/jCRy+N0R6NN/L/+LdAAjsodjzVmkLOUzPbZhw72hHXA63RijAhSDeg8IdCUY1YVygyr5KZpHRnn96XJOucPCCFWFfiToJ2Qz+JfZMBUt9HhsSWGS2jF2hUGjixhRwFwTd5xtYsCgYEA7DMe0bJeF336UtgrY9MNVC1sc/A8sEeYtlr47rcLlRDboaO9PRsuIMssxiZ/9uRkYP1FDmS3S8pgwg4UuoVUYHg5Tt95iq3aK0BRQnpKuGIerbBCwFUBZMwpP1DW3fMKtOQ5pjoV74tqR+df7gD0hWlfMFV8WUP9vXLa+XBcWqkCgYEA85sbw3IEsQ3UY9jTCSKzqy7NUQcgfGb8NmiwBa7QU08XUpDatMYgsAAdvCBYfeH11WL7X3+G0DZq+lfo3ZhWfbBiXtRb0t5YD2RqTCK75PtoO7PI95r1lAuB4PtU4Ile/R4kL3jnNj4MNupFX0Y6qu/BetqxsIt4E1QfZ+t1BNcCgYBrDiCB2t5at3al5eSEsjvwU0Y8pj5bh5fnzwPU7pIJVkK12IkFETSvGGeKyBhnxszYSPLruyp455lDWy55+8RqlRMkdJWaDYI86EHsZ5FGUPKmtqUKl3yyOvbXA8TfhDDuHCMk/F7E2+OoA26vaS9q6H+EYLqjmvV+0Hf/ZrX1QQKBgGd8r9wi+fPG3IgGXHBQjmnVgaPNsvQzBKFmHrER0/iLZuA9A2R5x7DxZdHUSRWaPADIaHiU1O9jbNJCk7Jtnqn7M85Q0SRsqZhA2+28/1bmqrTkQmT7T9Q4+hUEN+qehZx83BkRYaP1QWuH11UcRxFr+O3HNXlC9mAG/zt6HhuV');
+        $sig = hash_hmac('SHA1', $date, $key, true);
+        $sig = base64_encode($sig);
+        return $sig;
     }
