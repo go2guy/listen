e6b44426b524604df3ef9b8b80259c0208d61809
rob <rob.hruska@newnet.com>
2010-11-09 22:33:15 +0000

    Added tests for a couple servlets.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1404 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/src/test/java/com/interact/listen/gui/SaveAttendantMenuServletTest.java
+++ controller/src/test/java/com/interact/listen/gui/SaveAttendantMenuServletTest.java
@@ -1,13 +1,19 @@
 package com.interact.listen.gui;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import com.interact.listen.ListenServletTest;
 import com.interact.listen.ServletUtil;
 import com.interact.listen.TestUtil;
+import com.interact.listen.attendant.Menu;
 
 import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.junit.Test;
 
 public class SaveAttendantMenuServletTest extends ListenServletTest
@@ -29,5 +35,52 @@ public class SaveAttendantMenuServletTest extends ListenServletTest
         TestUtil.setSessionSubscriber(request, false, session);
         request.setMethod("POST");
         testForListenServletException(servlet, 401, "Unauthorized - Insufficient permissions");
+    }
+
+    @Test
+    public void test_doPost_withValidNewMenuAndNotNamedTopMenu_savesMenu() throws ServletException, IOException
+    {
+        TestUtil.setSessionSubscriber(request, true, session);
+
+        String name = TestUtil.randomString();
+
+        JSONObject json = new JSONObject();
+        json.put("name", name);
+        json.put("audioFile", TestUtil.randomString());
+
+        JSONObject defaultAction = new JSONObject();
+        defaultAction.put("action", "DialPressedNumber");
+        json.put("defaultAction", defaultAction);
+
+        JSONObject timeoutAction = new JSONObject();
+        timeoutAction.put("action", "DialPressedNumber");
+        json.put("timeoutAction", timeoutAction);
+
+        JSONObject dialNumberAction = new JSONObject();
+        dialNumberAction.put("action", "DialNumber");
+        JSONObject args = new JSONObject();
+        args.put("number", TestUtil.randomString());
+        dialNumberAction.put("arguments", args);
+
+        JSONArray actions = new JSONArray();
+        actions.add(dialNumberAction);
+
+        json.put("actions", actions);
+
+        request.setParameter("menu", json.toJSONString());
+        servlet.doPost(request, response);
+
+        assertEquals(200, response.getStatus());
+
+        boolean found = false;
+        for(Menu menu : Menu.queryAll(session))
+        {
+            found |= menu.getName().equals(name);
+        }
+        assertTrue(found);
+        
+        // TODO apparently queryByName isn't coming back with the menu, not sure why
+//        List<Menu> result = Menu.queryByName(session, name);
+//        assertEquals(0, result.size());
     }
 }
--- controller/src/test/java/com/interact/listen/gui/SetPropertiesServletTest.java
+++ controller/src/test/java/com/interact/listen/gui/SetPropertiesServletTest.java
@@ -51,4 +51,75 @@ public class SetPropertiesServletTest extends ListenServletTest
 
         assertEquals(value, Configuration.get(Property.Key.MAIL_FROMADDRESS));
     }
+
+    @Test
+    public void test_doPost_withUnrecognizedProperty_returns200() throws ServletException, IOException
+    {
+        // unrecognized properties are ignored, but 200 is returned
+        TestUtil.setSessionSubscriber(request, true, session);
+
+        String name = TestUtil.randomString();
+        String value = TestUtil.randomString();
+
+        request.setParameter(name, value);
+        servlet.doPost(request, response);
+
+        assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    public void test_doPost_withDnisMappingContainingEntryWithWildcardNotAtTheEnd_throwsBadRequest()
+        throws ServletException, IOException
+    {
+        TestUtil.setSessionSubscriber(request, true, session);
+
+        request.setParameter(Property.Key.DNIS_MAPPING.getKey(), "1*234:foo");
+        request.setMethod("POST");
+
+        testForListenServletException(servlet, 400, "Wildcard (*) may only be at the end of mapping 1*234");
+    }
+    
+    @Test
+    public void test_doPost_withDnisMappingContainingDuplicateEntry_throwsBadRequest() throws ServletException, IOException
+    {
+        TestUtil.setSessionSubscriber(request, true, session);
+
+        request.setParameter(Property.Key.DNIS_MAPPING.getKey(), "1234:foo;1234:bar");
+        request.setMethod("POST");
+
+        testForListenServletException(servlet, 400, "Mapping [1234] cannot be defined twice");
+    }
+    
+    @Test
+    public void test_doPost_withConferencingPinLengthLessThanThree_throwsBadRequest() throws ServletException, IOException
+    {
+        TestUtil.setSessionSubscriber(request, true, session);
+        
+        request.setParameter(Property.Key.CONFERENCING_PINLENGTH.getKey(), "2");
+        request.setMethod("POST");
+        
+        testForListenServletException(servlet, 400, "Conferencing PIN length [2] must be between 3 and 16 (inclusive)");
+    }
+
+    @Test
+    public void test_doPost_withConferencingPinLengthMoreThan16_throwsBadRequest() throws ServletException, IOException
+    {
+        TestUtil.setSessionSubscriber(request, true, session);
+        
+        request.setParameter(Property.Key.CONFERENCING_PINLENGTH.getKey(), "17");
+        request.setMethod("POST");
+        
+        testForListenServletException(servlet, 400, "Conferencing PIN length [17] must be between 3 and 16 (inclusive)");
+    }
+    
+    @Test
+    public void test_doPost_withConferencingPinLengthNotANumber_throwsBadRequest() throws ServletException, IOException
+    {
+        TestUtil.setSessionSubscriber(request, true, session);
+        
+        request.setParameter(Property.Key.CONFERENCING_PINLENGTH.getKey(), "asdf");
+        request.setMethod("POST");
+        
+        testForListenServletException(servlet, 400, "Conferencing PIN length [asdf] must be a number");
+    }
 }
