19503d543bdf082af51517180e0b65492793ea2c
ladi <ladi.ak@newnet.com>
2010-11-19 23:15:11 +0000

    Added php script to convert wav to mp3
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1432 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ spotapps/scripts/wavToMP3.php
@@ -0,0 +1,32 @@
+<?php
+    #Initialize Variables
+    $objName    = "appObj";
+    $PCM        = "";
+    $MP3        = "";
+    $tmpFile    = "";
+    
+    #Grab inputs
+    $inputFile      = @$_REQUEST['fileLocation']       or exitresult ($objName,$status,"'fileLocation' argument missing from request");
+
+    $tmpFile = substr($inputFile,0, -4);
+    $convertToPCM  = "/interact/listen/bin/logtolinwav < $inputFile > $tmpFile.pcm.wav";
+    $convertToMP3  = "/interact/listen/bin/lame $tmpFile.pcm.wav $tmpFile.mp3";
+
+    // Execute 
+    exec("$convertToPCM", $output, $return_var);
+    $PCM = "$return_var";
+
+    exec("$convertToMP3", $output, $return_var);
+    $MP3 = "$return_var";
+
+    exitresult($objName,$PCM,$MP3);
+    
+    #Exit php
+    function exitresult ($objname, $PCM, $MP3) {
+        echo "<?xml version=\"1.0\"?>\n";
+        echo "<{$objname}>\n";
+        echo "  <pcm>{$PCM}</pcm>\n";
+        echo "  <mp3>{$MP3}</mp3>\n";
+        echo "</{$objname}>\n";
+        exit;
+    }
