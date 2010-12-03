47e532b76311c28340d4ca40c5d0a8f5315adff0
austins <austin.shoemaker@newnet.com>
2010-12-03 17:46:48 +0000

    GetAudioFileServlet (android audio download): set the response content length from the content length of the connection to the IVR
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@1487 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/src/main/java/com/interact/listen/api/GetAudioFileServlet.java
+++ controller/src/main/java/com/interact/listen/api/GetAudioFileServlet.java
@@ -13,14 +13,11 @@ import com.interact.listen.history.HistoryService;
 import com.interact.listen.license.License;
 import com.interact.listen.license.ListenFeature;
 import com.interact.listen.license.NotLicensedException;
-import com.interact.listen.resource.Audio;
 import com.interact.listen.resource.Subscriber;
 import com.interact.listen.resource.Voicemail;
 import com.interact.listen.stats.Stat;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -30,6 +27,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
@@ -50,14 +48,40 @@ public class GetAudioFileServlet extends HttpServlet
             throw new NotLicensedException(ListenFeature.VOICEMAIL);
         }
         
+        request.setAttribute(OutputBufferFilter.OUTPUT_SUPPRESS_KEY, Boolean.TRUE);
+
         ServletUtil.sendStat(request, Stat.GUI_DOWNLOAD_VOICEMAIL);
 
-        Authentication authentication = (Authentication)request.getAttribute(AuthenticationFilter.AUTHENTICATION_KEY); 
-        Subscriber subscriber = authentication.getSubscriber();
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         
-        if(subscriber == null)
+        Subscriber subscriber = null;
+        
+        Authentication auth = (Authentication)request.getAttribute(AuthenticationFilter.AUTHENTICATION_KEY); 
+        if(auth != null)
         {
-            throw new UnauthorizedServletException("Not logged in");
+            if(auth.getType() == AuthenticationFilter.AuthenticationType.SUBSCRIBER && auth.getSubscriber() != null)
+            {
+                subscriber = auth.getSubscriber();
+            }
+            if(subscriber == null)
+            {
+                throw new UnauthorizedServletException("Not logged in");
+            }
+        }
+        else
+        {
+            // for testing purposes, we are going to pull the user name out of the authentication header
+            String subHeader = request.getHeader("X-Listen-AuthenticationUsername");
+            if(subHeader != null)
+            {
+                String username = new String(Base64.decodeBase64(subHeader));
+                subscriber = Subscriber.queryByUsername(session, username);
+    
+                if(subscriber == null)
+                {
+                    throw new UnauthorizedServletException("Unable to find subscriber from encoded header [" + subHeader + "]");
+                }
+            }
         }
 
         String id = ((HttpServletRequest)request).getPathInfo().substring(1);
@@ -67,8 +91,8 @@ public class GetAudioFileServlet extends HttpServlet
         }
         
         String acceptType = request.getHeader("Accept");
-        
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        boolean doesAcceptMp3 = acceptType != null && acceptType.equals(MP3_FILE_TYPE);
+
         PersistenceService persistenceService = new DefaultPersistenceService(session, subscriber, Channel.TUI);
 
         Voicemail voicemail = (Voicemail)session.get(Voicemail.class, Long.valueOf(id));
@@ -77,27 +101,33 @@ public class GetAudioFileServlet extends HttpServlet
 
         try
         {
-            // input
-            URL url = null;
-            if(acceptType != null && acceptType.equals(MP3_FILE_TYPE))
+            String audioUri = null;
+            if(doesAcceptMp3)
             {
-                url = ServletUtil.encodeUri(voicemail.mp3FileUri());
+                audioUri = voicemail.mp3FileUri();
             }
             else
             {
-                url = ServletUtil.encodeUri(voicemail.getUri());
+                audioUri = voicemail.getUri();
             }
+            URL url = ServletUtil.encodeUri(audioUri);
+            LOG.debug("Getting Audio File: " + url);
+
+            HistoryService historyService = new DefaultHistoryService(persistenceService);
+            historyService.writeDownloadedVoicemail(voicemail);
             
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
-            input = connection.getInputStream();
 
-            // output
-            OutputStream output = response.getOutputStream();
-            response.setContentLength(Integer.parseInt(voicemail.getFileSize()));
-            
-            if(acceptType != null && acceptType.equals(MP3_FILE_TYPE))
+            String fileName = audioUri.substring(audioUri.lastIndexOf("/") + 1);
+            response.setHeader("Content-disposition", "attachment; filename=" + fileName);
+
+            int fileSize = connection.getContentLength();
+            LOG.debug("Audio Content Length " + fileSize + " {" + voicemail.getFileSize() + "}");
+            response.setContentLength(fileSize);
+
+            if(doesAcceptMp3)
             {
                 response.setContentType(MP3_FILE_TYPE);
             }
@@ -106,12 +136,8 @@ public class GetAudioFileServlet extends HttpServlet
                 response.setContentType(WAV_FILE_TYPE);
             }
             
-            response.setHeader("Content-disposition", "attachment; filename=" + getFileName(voicemail));
-
-            HistoryService historyService = new DefaultHistoryService(persistenceService);
-            historyService.writeDownloadedVoicemail(voicemail);
-
-            request.setAttribute(OutputBufferFilter.OUTPUT_SUPPRESS_KEY, Boolean.TRUE);
+            input = connection.getInputStream();
+            OutputStream output = response.getOutputStream();
             IOUtils.copy(input, output);
         }
         catch(MalformedURLException e)
@@ -134,10 +160,5 @@ public class GetAudioFileServlet extends HttpServlet
             }
         }
     }
-
-    private String getFileName(Audio audio)
-    {
-        String uri = audio.getUri();
-        return uri.substring(uri.lastIndexOf("/") + 1);
-    }
+    
 }
