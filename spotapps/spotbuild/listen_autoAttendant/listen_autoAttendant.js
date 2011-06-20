944e3b8719795cce5f5a2c71607a765cfb5b4a5d
ladi <ladi.ak@newnet.com>
2011-06-20 21:17:39 +0000

    Merged HQ listen_autoAttendant to trunk
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2407 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/spotbuild/listen_autoAttendant/listen_autoAttendant.js
+++ spotapps/spotbuild/listen_autoAttendant/listen_autoAttendant.js
@@ -1,15 +1,70 @@
 function setControllerURL(appObj) {
     var tmpVal = eval("("+appObj+")");
-    var result = tmpVal.cntrlURL.split('/api');
-    return result[0] + "/meta";
+    return changeSuffix(tmpVal.cntrlURL,'/','meta');
 }
 
 function getMenuHotKeys(menuObj) {
     var result = '';
     var tmpVal = getJsonVal(menuObj, 'args');
+    var wildCard = '?';
     for (var i=0; i<tmpVal.keyPresses.length; i++) {
-        if (tmpVal.keyPresses[i].length == 1)
-            result = result + tmpVal.keyPresses[i] + ',';
+        if (tmpVal.keyPresses[i].length == 1) {
+            if (tmpVal.keyPresses[i] == wildCard)
+                result = result + '1,2,3,4,5,6,7,8,9,0,#,*,';
+            else
+                result = result + tmpVal.keyPresses[i] + ',';
+        }
     }
     return result;
+}
+
+function getDtmfLength(menuObj) {
+    var max = 1;
+    var index = 0;
+    var result = getJsonVal(menuObj, 'args');
+    var list = result.keyPresses;
+    while (index < list.length) {
+        if (list[index].length > max)
+            max = list[index].length;
+        index++;
+    }
+    return max;
+}
+
+function setOutBoundID (phoneNumber, passValues, ANI) {
+    var str = new String(phoneNumber);
+    var num = /^\d+$/;
+    var tmpSipURL = getJsonVal(passValues, 'sipURL');
+    var tmpPstn = getJsonVal(passValues, 'pstnLength');
+    if ((num.test(str)) && (phoneNumber.length >= tmpPstn) && (tmpSipURL.length > 0))
+        return ANI + "@" + tmpSipURL;
+    else
+        return ANI + getJsonVal(passValues, 'EXT_SUFFIX');
+}
+
+function setDestValue (phoneNumber, passValues) {
+    var str = new String(phoneNumber);
+    var num = /^\d+$/;
+    if (num.test(str)) {
+        var tmpSipURL = getJsonVal(passValues, 'sipURL');
+        var tmpPstn = getJsonVal(passValues, 'pstnLength');
+        var tmpExtLength = getJsonVal(passValues, 'EXT_LENGTH');
+        if ((phoneNumber.length >= tmpPstn) && (tmpSipURL.length > 0))
+            return phoneNumber + "@" + tmpSipURL;
+        else if (phoneNumber.length < tmpExtLength)
+            return getJsonVal(passValues, 'EXT_PREFIX') + phoneNumber + getJsonVal(passValues, 'EXT_SUFFIX');
+        else
+            return phoneNumber;
+    }
+    else
+        return phoneNumber;
+}
+
+function setParamsForCallEnd(sysAccessTime, callType, ANI, DNIS, callResult, organization, callEndTime) {
+    var duration = callEndTime - sysAccessTime;
+    if(callEndTime == '0')
+    {
+    duration = 0;
+    }
+    return "{\"date\": \""+iiDateToISO(sysAccessTime)+"\", \"service\": \""+callType+"\", \"duration\":"+duration+", \"ani\":\""+ANI+"\", \"dnis\":\""+DNIS+"\", \"result\":\""+callResult+"\", \"organization\": {\"href\":\""+organization+"\"}}";
 }
