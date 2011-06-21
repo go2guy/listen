f0d3a275e4bd15ee21bbded91cdfce885db420fa
ladi <ladi.ak@newnet.com>
2011-06-21 12:45:24 +0000

    Merged HQ listen_findmeAdmin to trunk
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2419 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/spotbuild/listen_findmeAdmin/listen_findmeAdmin.js
+++ spotapps/spotbuild/listen_findmeAdmin/listen_findmeAdmin.js
@@ -1,15 +1,23 @@
+function setParamsForCallEndCaller(startTime, endTime, callType, ANI, DNIS, organization) {
+    var duration = endTime - startTime;
+    return "{\"date\": \""+iiDateToISO(startTime)+"\", \"service\": \""+callType+"\", \"duration\":"+duration+", \"ani\":\""+ANI+"\", \"dnis\":\""+DNIS+"\", \"organization\": {\"href\":\""+organization+"\"}}";
+}
+
 function findMeConfigCount(findMeConfigObj, index) {
+    var configCount = 0;
     var result = eval("("+findMeConfigObj+")");
     var num = /^\d+$/;
-    var configCount = result.length;
+    if (typeof(result.results.length) != 'undefined') {
+        configCount = result.results.length;
+    }
     if ((num.test(index)) && (index < configCount)) {
-        configCount = result[index].length;
+        configCount = result.results[index].length;
     }
     if (configCount == 1) {
         if (index == 'all') {
             index = 0;
         }
-        var hrefID = getNextElement('L',result[index][0].href,'/');
+        var hrefID = getNextElement('L',result.results[index][0].href,'/');
         if (hrefID == 'null') {
             configCount = 0;
         }
@@ -21,10 +29,14 @@ function getFindMeConfiguartion(findMeConfigObj, configIndex, groupIndex, flag) 
     var tmpObj = eval("("+findMeConfigObj+")");
     var num = /^\d+$/;
     var result = '';
-    var configCount = tmpObj.length;
+    var configCount = 0;
+    if (typeof(tmpObj.results.length) != 'undefined') {
+        configCount = tmpObj.results.length;
+    }
+
     if ((num.test(configIndex)) && (num.test(groupIndex))) {
-        if ((configIndex < tmpObj.length) && (groupIndex < tmpObj[configIndex].length)) {
-            var tmpVal = tmpObj[configIndex][groupIndex][flag];
+        if ((configIndex < tmpObj.results.length) && (groupIndex < tmpObj.results[configIndex].length)) {
+            var tmpVal = tmpObj.results[configIndex][groupIndex][flag];
             if ((typeof(tmpVal) != 'undefined') && (tmpVal != 'null'))
                 result = tmpVal;
         }
@@ -32,51 +44,19 @@ function getFindMeConfiguartion(findMeConfigObj, configIndex, groupIndex, flag) 
     return result;
 }
 
-function checkNumberStatus(status, keyPress) {
-    var result = 'false';
-    if (status == keyPress)
-        result = 'Play Status';
-    else {
-        if (keyPress == 1)
-            result = 'true';
-    }
-    return result;
+function setStatus(keyPress) {
+    var numberStatus = false;
+    if (keyPress == 1)
+        numberStatus = true;
+    return "{\"isEnabled\":" + numberStatus +"}"
 }
 
 function removeNumber(findMeConfigObj, configIndex, groupIndex) {
     var result = eval("("+findMeConfigObj+")");
-    result[configIndex].splice(groupIndex,1);
+    result.results[configIndex].splice(groupIndex,1);
     return iiStringify(result);
 }
 
-function isFindMeExpired(findMeObj) {
-    var isExpired = 'true';
-    var tmpVal = getResultsKeyValue(findMeObj, 0, 'findMeExpiration');
-    if (tmpVal.length == 0)
-        isExpired = 'false';
-    else if (typeof(tmpVal != 'undefined') && (tmpVal.length > 0)) {
-        var date = tmpVal.split('T')[0];
-        var time = tmpVal.split('T')[1].split('.')[0];
-        var year = date.split('-')[0];
-        var month = date.split('-')[1];
-        month = parseInt(month,10) - 1;
-        var day = date.split('-')[2];
-        var hrs = time.split(':')[0];
-        var mins = time.split(':')[1];
-        var secs = time.split(':')[2];
-        var expireTime = new Date(year, month, day, hrs, mins, secs);
-        var currTime = new Date();
-        if (expireTime > currTime)
-            isExpired = 'false';
-    }
-    return isExpired;
-}
-
 function resetConfig(isActivating) {
-    var addDay = 0;
-    var currTime = new Date().getTime();
-    if (isActivating == 'true')
-        addDay = 86400000;
-    currTime = currTime + addDay;
-    return "{\"findMeExpiration\":\"" + iiDateToISO(currTime)+"\"}"
+    return "{\"isActivating\":" + isActivating + "}";
 }
