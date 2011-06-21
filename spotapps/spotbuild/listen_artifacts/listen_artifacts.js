9fa02bd75923a07deff40d55abe2ff427d608073
ladi <ladi.ak@newnet.com>
2011-06-21 16:28:28 +0000

    TP Bug: 25723
    Fixed issues with deleting voicemail 'wav' files. Re-factored  listen_artifacts code
    
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2424 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/spotbuild/listen_artifacts/listen_artifacts.js
+++ spotapps/spotbuild/listen_artifacts/listen_artifacts.js
@@ -1,30 +1,11 @@
 function getArtifact(argList) {
-    var result ='';
-    var tmpVal = new String(argList).replace(/^\s*|\s*$/g,'');
-    if((tmpVal == '') || (typeof(argList) == 'undefined'))
-        result = "ERROR";
-    else {
-        tmpVal = eval("("+argList+")");
-        switch (tmpVal.action) {
-            case "FILE":
-                result = tmpVal.artifact.split(tmpVal.hostName)[1];
-                break;
-            case "SUB":
-                result = tmpVal.artifactsDIR + tmpVal.artifact;
-                break;
-            default:
-                result = "ERROR";
-                break;
-        }
-    }
-    return result;
+    return getJsonVal(argList, 'artifactsDIR') + getJsonVal(argList, 'artifact').split('artifacts/')[1];
 }
 
-function findMP3(argList) {
-    var result = 'exit';
-    var tmpVal = eval("("+argList+")");
-    if ((tmpVal.action == 'FILE') && (inString('voicemail',tmpVal.artifact,'/'))) {
-        result = tmpVal.artifact.split(tmpVal.hostName)[1].split('.wav')[0] + '.mp3';
-    }
-    return result
+function getFileName(artifact, fileType, index) {
+    var result = '';
+    var type = getNextElement(index, fileType, '|');
+    if (type != '-1')
+        result = artifact.slice(0,-3) + type;
+    return result;
 }
