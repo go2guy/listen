e72b8ae1118174e03e0d5039d0fe402fefbbfeca
ladi <ladi.ak@newnet.com>
2011-07-22 18:56:50 +0000

    Updated listen_main with fax changes
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2615 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/spotbuild/listen_main/listen_main.js
+++ spotapps/spotbuild/listen_main/listen_main.js
@@ -1,4 +1,14 @@
-function getAppName(appName) {
+function getAppName(appName, jsonObj) {
+    if ((typeof(jsonObj) != 'undefined') && (jsonObj.length > 0)) {
+        var application = new String(appName).replace(/^\s*|\s*$|\s+/g,'');
+        application = application.toUpperCase();
+
+        if (application != 'DIRECTMESSAGE') {
+            var licensed = getJsonVal(jsonObj, appName);
+            if (!licensed)
+                return '';
+        }
+    }
     switch (appName) {
         case "AUTO_DIAL":
             appName = "listen_autoDial";
@@ -40,14 +50,17 @@ function getAppName(appName) {
         case "IP PBX":
             appName = "ippbx";
             break;
+        case "Direct Message":
+            appName = "directMessage";
+            break;
         default:
     }
     return appName;
 }
 
-function extendAppObject(passValues,organization,appToAccess,cntrlURL,hostName,sysAccessTime,HTTPcontroller,sipURL,sipDirect,STATcontroller,artifactsDIR,EXT_LENGTH,pstnLength,EXT_PREFIX,EXT_SUFFIX) {
+function extendAppObject(passValues,organization,appToAccess,cntrlURL,hostName,sysAccessTime,HTTPcontroller,sipURL,sipDirect,STATcontroller,artifactsDIR,EXT_LENGTH,pstnLength,EXT_PREFIX,EXT_SUFFIX,licenses,viopChannel) {
     if ((typeof(passValues) == 'undefined') || (passValues.length == 0)) {
-        return "{\"application\":\""+appToAccess+"\", \"sysAccessTime\": \""+ sysAccessTime+"\", \"EXT_LENGTH\": \""+EXT_LENGTH+"\", \"EXT_PREFIX\": \""+EXT_PREFIX+"\", \"pstnLength\": \""+pstnLength+"\", \"EXT_SUFFIX\": \""+EXT_SUFFIX+"\", \"sipURL\":\""+sipURL+"\", \"sipDirect\":\""+sipDirect+"\", \"cntrlURL\":\""+cntrlURL+"/api\", \"hostName\":\""+hostName+"\", \"HTTPcontroller\": \""+ HTTPcontroller+"\", \"artifactsDIR\": \""+ artifactsDIR+"\", \"organization\": \""+ organization+"\", \"STATcontroller\": \""+ STATcontroller+"\"}";
+        return "{\"application\":\""+appToAccess+"\", \"licenses\":"+licenses+", \"voipChannel\":\""+viopChannel+"\", \"sysAccessTime\": \""+ sysAccessTime+"\", \"EXT_LENGTH\": \""+EXT_LENGTH+"\", \"EXT_PREFIX\": \""+EXT_PREFIX+"\", \"pstnLength\": \""+pstnLength+"\", \"EXT_SUFFIX\": \""+EXT_SUFFIX+"\", \"sipURL\":\""+sipURL+"\", \"sipDirect\":\""+sipDirect+"\", \"cntrlURL\":\""+cntrlURL+"/api\", \"hostName\":\""+hostName+"\", \"HTTPcontroller\": \""+ HTTPcontroller+"\", \"artifactsDIR\": \""+ artifactsDIR+"\", \"organization\": \""+ organization+"\", \"STATcontroller\": \""+ STATcontroller+"\"}";
     }
     else {
         var result = eval("("+passValues+")");
@@ -55,9 +68,8 @@ function extendAppObject(passValues,organization,appToAccess,cntrlURL,hostName,s
         result.hostName = hostName;
         result.sipURL = sipURL;
         result.sipDirect = sipDirect;
-        if(!result.sysAccessTime)
-        {        
-        result.sysAccessTime = sysAccessTime;
+        if(!result.sysAccessTime) {        
+            result.sysAccessTime = sysAccessTime;
         }
         result.HTTPcontroller = HTTPcontroller;
         result.STATcontroller = STATcontroller;
@@ -67,9 +79,8 @@ function extendAppObject(passValues,organization,appToAccess,cntrlURL,hostName,s
         result.EXT_PREFIX = EXT_PREFIX;
         result.EXT_SUFFIX = EXT_SUFFIX;
         result.application = appToAccess;
-        if(organization != '')
-        {
-        result.organization = organization;
+        if(organization != '') {
+            result.organization = organization;
         }
         return iiStringify(result);
     }
@@ -78,6 +89,8 @@ function extendAppObject(passValues,organization,appToAccess,cntrlURL,hostName,s
 function setRouteRequest(ANI, DNIS) {
     var ani = getnum(ANI);
     var dnis = getnum(DNIS);
+    if ((ani.length == 0 || (ani == undefined))
+        ani = 'anonymous';
     return "?ani=" + escape(ani) + "&dnis=" + escape(dnis);
 }
 
@@ -89,21 +102,4 @@ function getOrganization(jsonObj) {
     else
         result = "";
     return result;
-}
-
-function chkAppLicense(appName, organization) {
-    var result = new String(appName).replace(/^\s*|\s*$|\s+/g,'');
-    switch (result) {
-        case "Mailbox":
-        case "DirectVoicemail":
-        case "Voicemail":
-            result = "VOICEMAIL";
-            break;
-        case "FindMeConfig":
-            result = "FINDME";
-            break;
-        default:
-            result = result.toUpperCase();
-    }
-    return 'canAccessFeature?feature=' + result + '&organization=' + organization;
 }
