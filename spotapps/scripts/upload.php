1b945bffc2ec03682543b4b2da49979d7bcf3dd4
ladi <ladi.ak@newnet.com>
2011-07-22 19:15:16 +0000

    Added new php script to upload faxes
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2620 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ spotapps/scripts/upload.php
@@ -0,0 +1,30 @@
+<?php
+	$dest 	 = @$_REQUEST['dest'];
+	$destDir = @$_REQUEST['destDir'];
+
+    if (!file_exists($destDir)) {
+        if(!mkdir($destDir,0777,true))
+    		exitresult("Failure", "Unable to create $destDir");
+    }
+
+    foreach($_FILES AS $field => $value)
+    {
+        $dest = $destDir.$dest;
+        $real = $_FILES[$field]['name'];
+        if(move_uploaded_file($_FILES[$field]['tmp_name'], $dest)) {
+            chmod($dest,0777);
+            exitresult("Success", "");
+        }
+        else
+            exitresult("Failure", "Unable to move uploaded file from tmp directory to [$destDir]");
+    }
+
+	function exitresult($result, $reason="") {
+		echo "<?xml version=\"1.0\"?>\n";
+		echo "<uploads>\n";
+		echo "  <Result>{$result}</Result>\n";
+ 		echo "  <Reason>{$reason}</Reason>\n";
+		echo "</uploads>\n";
+		exit;
+	}
+?>
