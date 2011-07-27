a292419d4812c409eec55debd5c872ac467ee636
rob <rob.hruska@newnet.com>
2011-07-27 15:59:51 +0000

    Rolling back changes for automation coverage. Trying a different approach.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2658 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/listen/scripts/ExecutableWar.groovy
+++ controller/listen/scripts/ExecutableWar.groovy
@@ -4,7 +4,7 @@ includeTargets << grailsScript('_GrailsWar')
 
 warName = null
 
-target(executableWar: "Builds a self-executing war file") {
+target(main: "Builds a self-executing war file") {
     depends(clean, war)
 
     configureWarName()
@@ -12,15 +12,6 @@ target(executableWar: "Builds a self-executing war file") {
     ant.delete(dir: 'target/war-stage')
     ant.mkdir(dir: 'target/war-stage')
     ant.unwar(src: warName, dest: 'target/war-stage')
-
-    if(argsMap.containsKey('instrument')) {
-        ant.taskdef(classpathRef: 'grails.test.classpath', resource: 'tasks.properties')
-        ant.'cobertura-instrument'(datafile: "cobertura.ser") {
-            fileset(dir: 'target/war-stage') {
-                include(name: "**/*.class")
-            }
-        }
-    }
 
     ant.unjar(dest: 'target/war-stage') {
         patternset {
@@ -45,4 +36,4 @@ target(executableWar: "Builds a self-executing war file") {
     ant.delete(dir: 'target/war-stage')
 }
 
-setDefaultTarget(executableWar)
+setDefaultTarget(main)
