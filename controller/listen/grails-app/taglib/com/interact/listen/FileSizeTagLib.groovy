4106610106df88da59fd6fd67ec12ad3bb0b2af6
tony <tony.wilbrand@newnet.com>
2011-07-29 20:01:05 +0000

    TP bug 26560.  Fax size will be shown as KB if it's less than 1 MB.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2680 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/listen/grails-app/taglib/com/interact/listen/FileSizeTagLib.groovy
+++ controller/listen/grails-app/taglib/com/interact/listen/FileSizeTagLib.groovy
@@ -4,6 +4,7 @@ class FileSizeTagLib {
     static namespace = 'listen'
 
     static final def BYTES_PER_MEGABYTE = 1024 * 1024
+    static final def BYTES_PER_KILOBYTE = 1024
 
     def megabytes = { attrs ->
         def bytes
@@ -13,7 +14,10 @@ class FileSizeTagLib {
             bytes = attrs.bytes as int
         }
 
-        if(bytes) {
+        if(bytes && bytes < BYTES_PER_MEGABYTE) {
+            def kilo = bytes / BYTES_PER_KILOBYTE
+            out << g.formatNumber(number: kilo, format: '#,##0.0') + ' KB'
+        } else if (bytes && bytes >= BYTES_PER_MEGABYTE) {
             def mega = bytes / BYTES_PER_MEGABYTE
             out << g.formatNumber(number: mega, format: '#,##0.0') + ' MB'
         } else {
