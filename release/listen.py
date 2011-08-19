f3f9b4d5b7ae9ad538e3e37f2f39d2606cb8f88f
joe <joe.ebemier@newnet.com>
2011-08-19 18:34:35 +0000

    Changes to bring in line with autopack updates.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2789 4692729f-32a0-46b7-9832-1bf53c4881b5

--- release/listen.py
+++ release/listen.py
@@ -3,34 +3,7 @@ import interface
 import os
 
 class listenAction(interface.Action):
-    INCLUDED_PACKAGES = [
-        interface.Package('all',
-            [interface.Process('httpd',
-                               '0',
-                               statusCmd='/sbin/service httpd status',
-                               statusSource=interface.Process.RETVAL_STATUS_SOURCE,
-                               stopCmd='/sbin/service httpd stop',
-                               startCmd='/sbin/service httpd start'),
-
-             interface.Process('vipStart',
-                               '/interact/program/iiMoap',
-                               stopCmd='/sbin/service vipStart stop',
-                               startCmd='/sbin/service vipStart start'),
-
-             interface.Process('listen-controller',
-                               '/interact/listen/lib/listen-controller.war',
-                               stopCmd='/sbin/service listen-controller stop',
-                               startCmd='/sbin/service listen-controller start'),
-
-             interface.Process('collector',
-                               '/interact/collector/bin/collector',
-                               stopCmd='/sbin/service collector stop',
-                               startCmd='/sbin/service collector start'),
-
-             interface.Process('mysql',
-                               '/usr/lib.*/mysqld ',
-                               startCmd='/sbin/service mysqld start',
-                               desiredState=interface.Process.RUNNING_STATE)])]
+    INCLUDED_PACKAGES = ['all']
 
     def __init__(self, masterpkg, uiapkg):
         super(listenAction, self).__init__(masterpkg, uiapkg)
