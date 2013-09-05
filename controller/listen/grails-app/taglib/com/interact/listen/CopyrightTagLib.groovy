fc1716cbf3aa793d9fe00238b36a5b9817e8d931
brian <brian.johnston@newnet.com>
2013-09-05 12:42:38 +0000

    fixing a failing unit test regarding copyright notice.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2919 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/listen/grails-app/taglib/com/interact/listen/CopyrightTagLib.groovy
+++ controller/listen/grails-app/taglib/com/interact/listen/CopyrightTagLib.groovy
@@ -6,6 +6,6 @@ class CopyrightTagLib {
     static namespace = 'listen'
 
     def copyright = { attrs ->
-        out << "Listen &copy;2012-${new DateTime().getYear()} NewNet Communication Technologies, <a href='http://www.newnet.com' title='NewNet'>newnet.com</a>"
+        out << "Listen &copy;2012-${new DateTime().getYear()} NewNet Communication Technologies, <a href='http://www.newnet.com' title='NewNet Communication Technologies'>newnet.com</a>"
     }
 }
