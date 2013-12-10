5f0c1ffd5c7700fffdad14b53f085e5682559eef
dwalter <daniel.walter@newnet.com>
2013-12-10 17:34:12 +0000

    Add Acd controller
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@2973 4692729f-32a0-46b7-9832-1bf53c4881b5

---
+++ controller/listen/grails-app/controllers/com/interact/listen/acd/AcdController.groovy
@@ -0,0 +1,37 @@
+package com.interact.listen.acd
+
+import grails.plugins.springsecurity.Secured
+import com.interact.listen.acd.Status
+
+@Secured(['ROLE_ACD_USER'])
+class AcdController {
+    static allowedMethods = [
+        index: 'GET',
+        status: 'POST',
+        updateStatus: 'POST'
+    ]
+
+    def index = {
+        redirect(action: 'status')
+    }
+
+    def updateStatus = {
+        log.debug("params.status [${params.status}]")
+        def cu = authenticatedUser
+        cu.status = Status.findByName(params.status)
+        cu.save(flush: true)
+        redirect(action: 'status')
+    }
+
+    def status = {
+        def status = authenticatedUser?.status?.name
+        def options = Status.findAll()
+        def option_names = []
+
+        options.each() { option ->
+          option_names.add(option.name)
+        }
+
+        render(view: 'status', model: [status: status, option_names: option_names])
+    }
+}
