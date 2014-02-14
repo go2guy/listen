82a100f1b240f058c1f80ddc6317a2bb1e25801c
ladi <ladi.ak@newnet.com>
2014-02-14 05:16:21 +0000

    Updates to fix acd disconnect issues
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@3104 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/spotbuild/listen_acd/listen_acd.js
+++ spotapps/spotbuild/listen_acd/listen_acd.js
@@ -1,15 +1,16 @@
 function setDestination (jsonObj) {
-	var destination = getJsonVal(jsonObj, 'number');
-	var extLength = getJsonVal(jsonObj, 'EXT_LENGTH');
-	var extPrefix = getJsonVal(jsonObj, 'EXT_PREFIX');
-	var extSuffix = getJsonVal(jsonObj, 'EXT_SUFFIX');
-	var sipUrl = getJsonVal(jsonObj, 'sipURL');
-	
-	if (destination.length > extLength) {
-		destination = destination + '@' + sipUrl;
-	} else {
-		destination = extPrefix + destination + extSuffix;
-	}
-	return destination;
+    var destination = getJsonVal(jsonObj, 'number');
+    var extLength = getJsonVal(jsonObj, 'EXT_LENGTH');
+    var extPrefix = getJsonVal(jsonObj, 'EXT_PREFIX');
+    var extSuffix = getJsonVal(jsonObj, 'EXT_SUFFIX');
+    var sipUrl = getJsonVal(jsonObj, 'sipURL');
+    
+    if (destination.length > extLength) {
+        destination = destination + '@' + sipUrl;
+    } else {
+        destination = extPrefix + destination + extSuffix;
+    }
+    return destination;
 }
+
 
