d5dcb2fb3e4c83575168f71d5e22ff9c21db35fc
kris <kris.knovak@newnet.com>
2014-07-25 18:05:59 +0000

    TP 66309 - Modify controller to send request to VEX.
    git-svn-id: svn+ssh://cvs1/svn/listen/branches/v7@3144 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/listen/grails-app/services/com/interact/listen/spot/SpotCommunicationService.groovy
+++ controller/listen/grails-app/services/com/interact/listen/spot/SpotCommunicationService.groovy
@@ -7,102 +7,122 @@ import com.interact.listen.stats.Stat
 import grails.converters.JSON
 import org.joda.time.format.DateTimeFormat
 
-class SpotCommunicationService {
+class SpotCommunicationService
+{
+    private static final String LISTEN_ORIGIN = "Listen_Controller";
+
+    private static final String CUSTOM_EVENT = "customEvent";
+    private static final String SESSION_ID = "sessionId";
+    private static final String ORIGIN = "origin";
+    private static final String ARGS = "args";
+    private static final String INITIATING_SUBSCRIBER = "initiatingSubscriber";
+
+    private static final String DROP_CALL = "DROP";
+    private static final String MUTE_CALL = "MUTE";
+    private static final String DIAL = "DIAL";
+    private static final String INTERACTIVE_DIAL = "INTERACTIVE_DIAL";
+    private static final String START_CONFERENCE = "START";
+    private static final String STOP_CONFERENCE = "STOP";
+    private static final String UNMUTE_PARTICIPANT = "UNMUTE";
+    private static final String FAX = "PDF_TO_TIFF";
+    private static final String ACD_CONNECT = "CONNECT";
+    private static final String ACD_DISCONNECT = "DISCONNECT";
+    private static final String ACD_VOICEMAIL = "VOICEMAIL";
+    private static final String ACD_HOLD = "ON_HOLD";
+    private static final String ACD_OFFHOLD = "OFF_HOLD";
+    private static final String ACD_SWITCH_QUEUE = "SWITCH_QUEUE";
+
     static transactional = false
 
     def grailsApplication
     def springSecurityService
     def statWriterService
 
-    def dropParticipant(def participant) throws IOException, SpotCommunicationException {
-        sendConferenceParticipantEvent("DROP", participant);
-        statWriterService.send(Stat.SPOT_CONF_EVENT_DROP)
+    def dropParticipant(def participant) throws IOException, SpotCommunicationException
+    {
+        sendConferenceParticipantEvent(DROP_CALL, participant);
+        statWriterService.send(Stat.SPOT_CONF_EVENT_DROP);
     }
 
-    def muteParticipant(def participant) throws IOException, SpotCommunicationException {
-        sendConferenceParticipantEvent("MUTE", participant);
+    def muteParticipant(def participant) throws IOException, SpotCommunicationException
+    {
+        sendConferenceParticipantEvent(MUTE_CALL, participant);
         statWriterService.send(Stat.SPOT_CONF_EVENT_MUTE)
     }
 
-    def outdial(def numbers, def conference, def requestingNumber) throws IOException, SpotCommunicationException {
-        Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "AUTO_DIAL");
-        importedValue.put("action", "DIAL");
-        importedValue.put("sessionId", conference.firstAdminSessionId());
-        importedValue.put("conferenceId", conference.id);
-        importedValue.put("destination", numbers);
-        importedValue.put("organization", "/organizations/${conference.owner.organization.id}")
-        importedValue.put("ani", requestingNumber);
-        buildAndSendRequest(importedValue);
+    def outdial(def numbers, def conference, def requestingNumber) throws IOException, SpotCommunicationException
+    {
+        Map<String, Object> args = new TreeMap<String, Object>();
+        args.put("conferenceId", conference.id);
+        args.put("destination", numbers);
+        args.put("organization", "/organizations/${conference.owner.organization.id}")
+        args.put("ani", requestingNumber);
+
+        buildAndSendRequest(DIAL, conference.firstAdminSessionId(), args);
         statWriterService.send(Stat.SPOT_AUTO_DIAL_DIAL)
     }
 
-    def interactiveOutdial(def numbers, def conference, def requestingNumber) throws IOException, SpotCommunicationException
+    def interactiveOutdial(def numbers, def conference, def requestingNumber)
+        throws IOException, SpotCommunicationException
     {
-        Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "CONF_EVENT");
-        importedValue.put("action", "INTERACTIVE_DIAL");
-        importedValue.put("sessionId", conference.firstAdminSessionId());
-        importedValue.put("destination", numbers);
-        importedValue.put("ani", requestingNumber);
-        importedValue.put("organization", "/organizations/${conference.owner.organization.id}")
-        buildAndSendRequest(importedValue);
+        Map<String, Object> args = new TreeMap<String, Object>();
+        args.put("destination", numbers);
+        args.put("ani", requestingNumber);
+        args.put("organization", "/organizations/${conference.owner.organization.id}")
+
+        buildAndSendRequest(INTERACTIVE_DIAL, conference.firstAdminSessionId(), args);
         statWriterService.send(Stat.SPOT_CONF_EVENT_BRIDGE_DIAL)
     }
 
-    def startRecording(def conference) throws IOException, SpotCommunicationException {
-        sendConferenceRecordingEvent("START", conference);
+    def startRecording(def conference) throws IOException, SpotCommunicationException
+    {
+        sendConferenceRecordingEvent(START_CONFERENCE, conference);
         statWriterService.send(Stat.SPOT_RECORD_START)
     }
 
     def stopRecording(def conference) throws IOException, SpotCommunicationException {
-        sendConferenceRecordingEvent("STOP", conference);
+        sendConferenceRecordingEvent(STOP_CONFERENCE, conference);
         statWriterService.send(Stat.SPOT_RECORD_STOP)
     }
 
-    def toggleMessageLight(def number, def ip, boolean on) throws IOException, SpotCommunicationException {
+    def toggleMessageLight(def number, def ip, boolean on) throws IOException, SpotCommunicationException
+    {
         sendMessageLightEvent(on ? 'ON' : 'OFF', number, ip)
         statWriterService.send(on ? Stat.SPOT_MSG_LIGHT_ON : Stat.SPOT_MSG_LIGHT_OFF)
     }
 
-    def unmuteParticipant(def participant) throws IOException, SpotCommunicationException {
-        sendConferenceParticipantEvent("UNMUTE", participant);
+    def unmuteParticipant(def participant) throws IOException, SpotCommunicationException
+    {
+        sendConferenceParticipantEvent(UNMUTE_PARTICIPANT, participant);
         statWriterService.send(Stat.SPOT_CONF_EVENT_UNMUTE)
     }
 
-    def sendConferenceParticipantEvent(def action, def participant) throws IOException, SpotCommunicationException {
+    def sendConferenceParticipantEvent(def action, def participant) throws IOException, SpotCommunicationException 
+    {
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "CONF_EVENT");
-        importedValue.put("action", action);
-        importedValue.put("sessionId", participant.sessionId);
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(action, participant.sessionId, importedValue);
     }
 
     def sendConferenceRecordingEvent(def action, def conference) throws IOException, SpotCommunicationException
     {
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "RECORD");
-        importedValue.put("action", action);
-        importedValue.put("sessionId", conference.firstAdminSessionId());
         importedValue.put("conferenceId", conference.id);
         importedValue.put("startTime", DateTimeFormat.forPattern('yyyyMMddhhmmssSSS').print(conference.startTime));
         importedValue.put("recordingSessionId", conference.recordingSessionId);
         importedValue.put("arcadeId", conference.arcadeId);
         importedValue.put("description", conference.description);
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(action, conference.firstAdminSessionId(), importedValue);
     }
 
     def sendFax(def fax) throws IOException, SpotCommunicationException
     {
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "Direct Message");
-        importedValue.put("action", "PDF_TO_TIFF");
         importedValue.put("artifact", fax.sourceFiles.collect{ it.file.absolutePath });
         importedValue.put("destination", fax.dnis);
         importedValue.put("ani", ""); //Will be implemented later, probably once sold and we know what to do
         importedValue.put("organization", "/organizations/${fax.sender.organization.id}");
         importedValue.put("id", fax.id)
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(FAX, null, importedValue);
     }
 
     /**
@@ -121,11 +141,9 @@ class SpotCommunicationService {
         }
 
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "ACD");
-        importedValue.put("action", "CONNECT");
-        importedValue.put("sessionId", sessionId);
         importedValue.put("number", number);
-        buildAndSendRequest(importedValue);
+
+        buildAndSendRequest(ACD_CONNECT, sessionId, importedValue);
     }
 
     /**
@@ -143,10 +161,7 @@ class SpotCommunicationService {
         }
 
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "ACD");
-        importedValue.put("action", "DISCONNECT");
-        importedValue.put("sessionId", sessionId);
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(ACD_DISCONNECT, sessionId, importedValue);
     }
 
     /**
@@ -165,10 +180,7 @@ class SpotCommunicationService {
         }
 
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "ACD");
-        importedValue.put("action", event);
-        importedValue.put("sessionId", sessionId);
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(event, sessionId, importedValue);
     }
 
     /**
@@ -187,11 +199,9 @@ class SpotCommunicationService {
         }
 
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "ACD");
-        importedValue.put("action", "VOICEMAIL");
-        importedValue.put("sessionId", sessionId);
         importedValue.put("number", number);
-        buildAndSendRequest(importedValue);
+
+        buildAndSendRequest(ACD_VOICEMAIL, sessionId, importedValue);
     }
 
     /**
@@ -209,10 +219,7 @@ class SpotCommunicationService {
         }
 
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "ACD");
-        importedValue.put("action", "ON_HOLD");
-        importedValue.put("sessionId", sessionId);
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(ACD_HOLD, sessionId, importedValue);
     }
 
     /**
@@ -230,10 +237,7 @@ class SpotCommunicationService {
         }
 
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "ACD");
-        importedValue.put("action", "OFF_HOLD");
-        importedValue.put("sessionId", sessionId);
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(ACD_OFFHOLD, sessionId, importedValue);
     }
 
     /**
@@ -252,34 +256,32 @@ class SpotCommunicationService {
         }
 
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "ACD");
-        importedValue.put("action", "SWITCH_QUEUE");
-        importedValue.put("sessionId", sessionId);
         importedValue.put("onHoldMsg", onHoldMsg);
         importedValue.put("onHoldMusic", onHoldMusic);
         importedValue.put("connectMsg", connectMsg);
         importedValue.put("onHoldMsgExtended", onHoldMsgExtended);
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(ACD_SWITCH_QUEUE, sessionId, importedValue);
     }
 
 
-    private void sendMessageLightEvent(def action, def number, def ip) throws IOException, SpotCommunicationException {
+    private void sendMessageLightEvent(def action, def number, def ip) throws IOException, SpotCommunicationException
+    {
         Map<String, Object> importedValue = new TreeMap<String, Object>();
-        importedValue.put("application", "MSG_LIGHT"); // monosodium glutimate light, on!
-        importedValue.put("action", action);
         importedValue.put("destination", number);
         importedValue.put('ip', ip)
-        buildAndSendRequest(importedValue);
+        buildAndSendRequest(action, null, importedValue);
     }
 
-    private void buildAndSendRequest(Map<String, Object> importedValue) throws IOException, SpotCommunicationException
+    private void buildAndSendRequest(String event, String sessionId, Map<String, Object> args) throws IOException, SpotCommunicationException
     {
+        Map<String, String> params = new TreeMap<String, String>();
+
         try
         {
-            def user = springSecurityService.getCurrentUser()
+            User user = (User)springSecurityService.getCurrentUser()
             if(user)
             {
-                importedValue.put("initiatingSubscriber", "${user.id}");
+                params.put(INITIATING_SUBSCRIBER, String.valueOf(user.id));
             }
         }
         catch(MissingPropertyException e)
@@ -287,18 +289,19 @@ class SpotCommunicationService {
             // handles a non-User principal
             if(log.isDebugEnabled())
             {
-                log.debug 'MissingPropertyException while building SPOT request, probably an API user'
+                log.debug("MissingPropertyException[" + e + "] while building SPOT request, probably an API user");
             }
         }
 
-        importedValue.put("initiatingChannel", Channel.GUI.toString());
-
-        Map<String, String> params = new TreeMap<String, String>();
-        params.put("uri", "/interact/apps/iistart.ccxml");
-//        def json = importedValue.encodeAsJSON()
-        def theJson = importedValue as JSON
-        String json = theJson.toString(false);
-        params.put("II_SB_importedValue", json);
+        params.put(CUSTOM_EVENT, event);
+        if(sessionId != null)
+        {
+            params.put(SESSION_ID, sessionId)
+        }
+        params.put(ORIGIN, LISTEN_ORIGIN);
+        def argsJson = args as JSON
+        String json = argsJson.toString(false);
+        params.put(ARGS, json);
         sendRequest(params);
     }
 
@@ -322,7 +325,8 @@ class SpotCommunicationService {
         sendRequest(params, spotUrls);
     }
 
-    private void sendRequest(Map<String, String> params, List<String> spotUrls) throws IOException, SpotCommunicationException
+    private void sendRequest(Map<String, String> params, List<String> spotUrls)
+        throws IOException, SpotCommunicationException
     {
         if(log.isDebugEnabled())
         {
@@ -336,8 +340,7 @@ class SpotCommunicationService {
         {
             def httpClient = new HttpClientImpl()
 
-            String uri = thisSpotUrl + "/ccxml/createsession";
-            httpClient.post(uri, params);
+            httpClient.post(thisSpotUrl, params);
 
             status = httpClient.getResponseStatus();
             if(!isSuccessStatus(status))
@@ -357,7 +360,9 @@ class SpotCommunicationService {
         }
     }
 
-    private boolean isSuccessStatus(int status) {
+    private boolean isSuccessStatus(int status)
+    {
         return status >= 200 && status <= 299;
     }
-} 
+}
+
