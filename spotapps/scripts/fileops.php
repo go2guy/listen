bd2d3ac02e172f3071c8f2a24df56aafb58eea21
brian <brian.johnston@newnet.com>
2015-03-09 19:15:39 +0000

    As part of conferencing testing, it was noted that a call to fileops.php to delete a file that does not exist returns an error, changed it to return success.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@3306 4692729f-32a0-46b7-9832-1bf53c4881b5

--- spotapps/scripts/fileops.php
+++ spotapps/scripts/fileops.php
@@ -1,103 +1,107 @@
 <?php
 
-	//-------------------------
+    //-------------------------
     // exitresult - return results and immediate exit
-	//-------------------------
-	function exitresult($objname, $result, $reason="", $size="", $accessed="", $modified="", $changed="") {
+    //-------------------------
+    function exitresult($objname, $result, $reason="", $size="", $accessed="", $modified="", $changed="") {
         header('Content-type: application/xml');
-		echo "<?xml version=\"1.0\"?>\n";
-		echo "<{$objname}>\n";
-		echo "  <Result>{$result}</Result>\n";
- 		echo "  <Reason>{$reason}</Reason>\n";
-		if ($size != "") 		echo "  <Size>{$size}</Size>\n";
-		if ($accessed != "") 	echo "  <LastAccess>{$accessed}</LastAccess>\n";
-		if ($modified != "") 	echo "  <LastModified>{$modified}</LastModified>\n";
-		if ($changed != "") 	echo "  <LastChanged>{$changed}</LastChanged>\n";
-		echo "</{$objname}>\n";
-		exit;
-	}
+        echo "<?xml version=\"1.0\"?>\n";
+        echo "<{$objname}>\n";
+        echo "  <Result>{$result}</Result>\n";
+        echo "  <Reason>{$reason}</Reason>\n";
+        if ($size != "")        echo "  <Size>{$size}</Size>\n";
+        if ($accessed != "")    echo "  <LastAccess>{$accessed}</LastAccess>\n";
+        if ($modified != "")    echo "  <LastModified>{$modified}</LastModified>\n";
+        if ($changed != "")     echo "  <LastChanged>{$changed}</LastChanged>\n";
+        echo "</{$objname}>\n";
+        exit;
+    }
 
-	// Inputs
-	$OBJNAME  	= @$_REQUEST['OBJNAME'] 	or $OBJNAME="fileOBJ";
-	$FILE1 		= @$_REQUEST['FILE1'] 		or exitresult($OBJNAME,"Failure","No FILE1 Argument");
-	$FILE2 		= @$_REQUEST['FILE2'] 		or $FILE2="";
-	$OPERATION 	= @$_REQUEST['OPERATION'] 	or exitresult($OBJNAME,"Failure","No OPERATION Argument");;
+    // Inputs
+    $OBJNAME    = @$_REQUEST['OBJNAME']     or $OBJNAME="fileOBJ";
+    $FILE1      = @$_REQUEST['FILE1']       or exitresult($OBJNAME,"Failure","No FILE1 Argument");
+    $FILE2      = @$_REQUEST['FILE2']       or $FILE2="";
+    $OPERATION  = @$_REQUEST['OPERATION']   or exitresult($OBJNAME,"Failure","No OPERATION Argument");;
 
-	// Handle the operations
-	switch ($OPERATION) {
+    // Handle the operations
+    switch ($OPERATION) {
 
-		case "stat":
+        case "stat":
             if (!(strpos($FILE1,'://'))) {
-			    $aStat = @stat($FILE1);
-			    if ($aStat)
-				    exitresult($OBJNAME, "Success", "", $aStat['size'], $aStat['atime'], $aStat['mtime'],$aStat['ctime']);
+                $aStat = @stat($FILE1);
+                if ($aStat)
+                    exitresult($OBJNAME, "Success", "", $aStat['size'], $aStat['atime'], $aStat['mtime'],$aStat['ctime']);
             }
             else {
                 if (@fopen($FILE1, 'r'))
-        		    exitresult($OBJNAME, "Success", "");
+                    exitresult($OBJNAME, "Success", "");
             }
-    	    exitresult($OBJNAME, "Failure", "Could not stat [$FILE1]");
-			break;
+            exitresult($OBJNAME, "Failure", "Could not stat [$FILE1]");
+            break;
 
-		case "copy":
-			$position = strrpos($FILE2, "/");
-			$DIRECTORY = substr($FILE2, 0, $position);
-			if(is_dir($DIRECTORY)){
-				if (copy($FILE1,$FILE2)) {
-					exitresult($OBJNAME, "Success", "");
-				} else {
-					exitresult($OBJNAME, "Failure", "Could not copy [$FILE1] to [$FILE2]");
-				}
-			}
-			else{
-				if(mkdir($DIRECTORY, 0777)){
-					if(copy($FILE1,$FILE2)){
-						exitresult($OBJNAME, "Success", "");
+        case "copy":
+            $position = strrpos($FILE2, "/");
+            $DIRECTORY = substr($FILE2, 0, $position);
+            if(is_dir($DIRECTORY)){
+                if (copy($FILE1,$FILE2)) {
+                    exitresult($OBJNAME, "Success", "");
+                } else {
+                    exitresult($OBJNAME, "Failure", "Could not copy [$FILE1] to [$FILE2]");
+                }
+            }
+            else{
+                if(mkdir($DIRECTORY, 0777)){
+                    if(copy($FILE1,$FILE2)){
+                        exitresult($OBJNAME, "Success", "");
 
-					}
-					else {
-						exitresult($OBJNAME, "Failure", "Could not copy [$FILE1] to [$FILE2]");			}
-				}
-				else{
-					exitresult($OBJNAME, "Failure", "Could not copy [$FILE1] to [$FILE2] FAILED to  make Directory");
-				}
-			}
-			break;
+                    }
+                    else {
+                        exitresult($OBJNAME, "Failure", "Could not copy [$FILE1] to [$FILE2]");         }
+                }
+                else{
+                    exitresult($OBJNAME, "Failure", "Could not copy [$FILE1] to [$FILE2] FAILED to  make Directory");
+                }
+            }
+            break;
 
-		case "delete":
+        case "delete":
             if ((is_dir($FILE1)) && (dltDir($FILE1))){
                 //Success
             }
-			elseif (@unlink($FILE1)) {
-                //Success
-			} else {
-    			exitresult($OBJNAME, "Failure", "Could not delete [$FILE1]");
+            elseif(is_file($FILE1) == true) {
+                @unlink($FILE1);
+                exitresult($OBJNAME, "Success", "File [$FILE1] deleted");
             }
-    		exitresult($OBJNAME, "Success", "");
-			break;
+            elseif (is_file($FILE1) == false) {
+                exitresult($OBJNAME, "Success", "File did not exist [$FILE1]");
+            } else {
+                exitresult($OBJNAME, "Failure", "Could not delete [$FILE1]");
+            }
+            exitresult($OBJNAME, "Success", "");
+            break;
 
-		case "rename":
-			if (@rename($FILE1,$FILE2)) {
-				exitresult($OBJNAME, "Success", "");
-			} else {
-				exitresult($OBJNAME, "Failure", "Could not rename [$FILE1] to [$FILE2]");
+        case "rename":
+            if (@rename($FILE1,$FILE2)) {
+                exitresult($OBJNAME, "Success", "");
+            } else {
+                exitresult($OBJNAME, "Failure", "Could not rename [$FILE1] to [$FILE2]");
             }
-			break;
+            break;
 
         case "create":
             if (@file_exists($FILE1)){
-				exitresult($OBJNAME, "Success", "Directory structure exists");
-			} else {
+                exitresult($OBJNAME, "Success", "Directory structure exists");
+            } else {
                 if(@mkdir($FILE1,0777,true)){
                     exitresult($OBJNAME, "Success", "Directory structure exists");
-			    } else {
-    				exitresult($OBJNAME, "Failure", "File path does not exist for file [$FILE1]");
+                } else {
+                    exitresult($OBJNAME, "Failure", "File path does not exist for file [$FILE1]");
                 }
             }
-			break;
+            break;
 
-		default:
-			exitresult($OBJNAME, "Failure", "Uknown OPERATION [$OPERATION]");
+        default:
+            exitresult($OBJNAME, "Failure", "Uknown OPERATION [$OPERATION]");
             break;
     }
 
