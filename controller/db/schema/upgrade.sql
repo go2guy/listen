e10d4f4d74f72fec6353528a99168957277972fe
kris <kris.knovak@newnet.com>
2015-03-30 15:03:31 +0000

    DB Schema updates.
    git-svn-id: svn+ssh://cvs1/svn/listen/branches/multitennant@3331 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/db/schema/upgrade.sql
+++ controller/db/schema/upgrade.sql
@@ -2,4 +2,10 @@ use listen2;
 
 ALTER TABLE sip_phone ADD COLUMN `user_agent` varchar(50) DEFAULT NULL AFTER `cseq`;
 
+alter table organization add column ad_server varchar(100);
+alter table organization add column ad_domain varchar(100);
+alter table organization add column ldap_basedn varchar(100);
+alter table organization add column ldap_port varchar(100);
+alter table organization add column ldap_dc varchar(100);
+
 commit work;
