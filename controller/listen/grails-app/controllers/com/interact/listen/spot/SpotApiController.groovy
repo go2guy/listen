35fbce225829a138e4f6bb1fef4549b4ac513ccf
cgeesey <corey.geesey@newnet.com>
2017-01-13 19:01:15 +0000

    TP #1781 rollback. Wrong place.
    git-svn-id: svn+ssh://cvs1/svn/listen/trunk@3492 4692729f-32a0-46b7-9832-1bf53c4881b5

--- controller/listen/grails-app/controllers/com/interact/listen/spot/SpotApiController.groovy
+++ controller/listen/grails-app/controllers/com/interact/listen/spot/SpotApiController.groovy
@@ -236,10 +236,8 @@ class SpotApiController {
 
         if(callHistory.validate() && callHistory.save()) {
             response.flushBuffer()
-	        statWriterService.send(Stat.SPOT_POST_CDR_SUCCESS)
         } else {
             response.sendError(HSR.SC_BAD_REQUEST, beanErrors(callHistory))
-	        statWriterService.send(Stat.SPOT_POST_CDR_FAILURE)
         }
     }
 
